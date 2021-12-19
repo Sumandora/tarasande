f1488405d77316fd6ffe05cccce62405d35c30a9
Sumandora <52950755+sumandora@users.noreply.github.com>
2021-12-19 16:26:57 +0100

    Adjust some configs

--- src/main/kotlin/su/mandora/tarasande/Entrypoint.kt
+++ src/main/kotlin/su/mandora/tarasande/Entrypoint.kt
@@ -1,9 +1,7 @@
 package su.mandora.tarasande
 
-import net.fabricmc.api.ClientModInitializer
-
-class Entrypoint : ClientModInitializer {
-    override fun onInitializeClient() {
+object Entrypoint {
+    fun onInitialize() {
         println("Hello, world!")
     }
 }
