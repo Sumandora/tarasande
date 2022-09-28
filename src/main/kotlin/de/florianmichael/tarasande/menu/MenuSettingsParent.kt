894192b3a6a3cacfd68a3c2e3f81ac687a0e9878
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-09-28 22:08:05 +0200

    Added: Menu Settings System

---
+++ src/main/kotlin/de/florianmichael/tarasande/menu/MenuSettingsParent.kt
@@ -0,0 +1,18 @@
+package de.florianmichael.tarasande.menu
+
+import de.florianmichael.tarasande.base.menu.ElementMenuScreen
+import de.florianmichael.tarasande.base.menu.ManagerMenu
+import su.mandora.tarasande.TarasandeMain
+import su.mandora.tarasande.value.ValueMode
+
+class MenuSettingsParent(managerMenu: ManagerMenu) {
+
+    var focusedMenuEntry: ValueMode? = null
+
+    init {
+        val entries = mutableListOf("None")
+        entries.addAll(managerMenu.list.filterIsInstance<ElementMenuScreen>().map { e -> e.name })
+
+        focusedMenuEntry = ValueMode(this, "Focused menu entry", false, *entries.toTypedArray())
+    }
+}
