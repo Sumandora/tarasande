8cee0a86588b2088fe67a1cf1e8a864ff3955ae2
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-09-29 19:45:33 +0200

    Added visibleInMenu option

--- src/main/kotlin/su/mandora/tarasande/screen/menu/panel/impl/elements/impl/category/PanelCategory.kt
+++ src/main/kotlin/su/mandora/tarasande/screen/menu/panel/impl/elements/impl/category/PanelCategory.kt
@@ -8,7 +8,7 @@ import su.mandora.tarasande.util.string.StringUtil
 class PanelCategory(private val moduleCategory: ModuleCategory, x: Double, y: Double) : PanelElements<ElementModule>(StringUtil.formatEnumTypes(moduleCategory.name), x, y, 150.0, 100.0) {
     init {
         TarasandeMain.get().managerModule.list.forEach {
-            if (it.category == moduleCategory)
+            if (it.category == moduleCategory && it.visibleInMenu)
                 elementList.add(ElementModule(it, 100.0))
         }
     }
