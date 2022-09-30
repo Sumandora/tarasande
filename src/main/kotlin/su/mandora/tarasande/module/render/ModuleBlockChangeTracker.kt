e203d25f3b7e39d51a4baa3eb34314084f803425
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-09-30 15:13:32 +0200

    Added custom time to ModuleBlockChangeTracker

--- src/main/kotlin/su/mandora/tarasande/module/render/ModuleBlockChangeTracker.kt
+++ src/main/kotlin/su/mandora/tarasande/module/render/ModuleBlockChangeTracker.kt
@@ -8,12 +8,15 @@ import su.mandora.tarasande.event.EventBlockChange
 import su.mandora.tarasande.event.EventRender3D
 import su.mandora.tarasande.util.render.RenderUtil
 import su.mandora.tarasande.value.ValueColor
+import su.mandora.tarasande.value.ValueNumber
 import java.awt.Color
 import java.util.function.Consumer
 
 class ModuleBlockChangeTracker : Module("Block change tracker", "Tracks block changes", ModuleCategory.RENDER) {
 
     private val color = ValueColor(this, "Color", 0.0f, 1.0f, 1.0f, 1.0f)
+    private val time = ValueNumber(this, "Time", 0.0, 1000.0, 10000.0, 500.0)
+
     private val hashMap = HashMap<EventBlockChange, Long>()
 
     val eventConsumer = Consumer<Event> { event ->
@@ -28,10 +31,10 @@ class ModuleBlockChangeTracker : Module("Block change tracker", "Tracks block ch
                 while (iterator.hasNext()) {
                     val entry = iterator.next()
                     val timeDelta = System.currentTimeMillis() - entry.value
-                    if (timeDelta > 1000L) {
+                    if (timeDelta > time.value) {
                         iterator.remove()
                     } else {
-                        RenderUtil.blockOutline(event.matrices, entry.key.state.getOutlineShape(mc.world, entry.key.pos).let { if (it.isEmpty) VoxelShapes.fullCube() else it }.offset(entry.key.pos.x.toDouble(), entry.key.pos.y.toDouble(), entry.key.pos.z.toDouble()), color.getColor().let { Color(it.red, it.green, it.blue, (it.alpha * (1.0f - timeDelta / 1000f)).toInt()) }.rgb)
+                        RenderUtil.blockOutline(event.matrices, entry.key.state.getOutlineShape(mc.world, entry.key.pos).let { if (it.isEmpty) VoxelShapes.fullCube() else it }.offset(entry.key.pos.x.toDouble(), entry.key.pos.y.toDouble(), entry.key.pos.z.toDouble()), color.getColor().let { Color(it.red, it.green, it.blue, (it.alpha * (1.0f - timeDelta / time.value)).toInt()) }.rgb)
                     }
                 }
             }
