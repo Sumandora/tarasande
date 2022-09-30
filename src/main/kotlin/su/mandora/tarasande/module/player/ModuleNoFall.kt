cb627a05883a2a8f0562f9dcdd74b00773650af3
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-09-30 19:37:43 +0200

    Nofall: New mode: Sneak-jump

--- src/main/kotlin/su/mandora/tarasande/module/player/ModuleNoFall.kt
+++ src/main/kotlin/su/mandora/tarasande/module/player/ModuleNoFall.kt
@@ -1,9 +1,11 @@
 package su.mandora.tarasande.module.player
 
+import net.minecraft.client.MinecraftClient
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 import su.mandora.tarasande.base.event.Event
 import su.mandora.tarasande.base.module.Module
 import su.mandora.tarasande.base.module.ModuleCategory
+import su.mandora.tarasande.event.EventKeyBindingIsPressed
 import su.mandora.tarasande.event.EventMovement
 import su.mandora.tarasande.event.EventPacket
 import su.mandora.tarasande.event.EventUpdate
@@ -16,7 +18,7 @@ import java.util.function.Consumer
 
 class ModuleNoFall : Module("No fall", "Prevents or reduces fall damage", ModuleCategory.PLAYER) {
 
-    private val mode = ValueMode(this, "Mode", false, "Ground spoof", "Re-Ground", "Drag down")
+    private val mode = ValueMode(this, "Mode", false, "Ground spoof", "Re-Ground", "Drag down", "Sneak-jump")
     private val groundSpoofMode = object : ValueMode(this, "Ground spoof mode", false, "Force on-ground", "Force off-ground") {
         override fun isEnabled() = mode.isSelected(0)
     }
@@ -27,35 +29,46 @@ class ModuleNoFall : Module("No fall", "Prevents or reduces fall damage", Module
         override fun isEnabled() = mode.isSelected(2)
     }
     private val resetFallDistance = object : ValueBoolean(this, "Reset fall distance", true) {
-        override fun isEnabled() = !mode.isSelected(2) && !(mode.isSelected(0) && groundSpoofMode.isSelected(1))
+        override fun isEnabled() = (mode.isSelected(0) && groundSpoofMode.isSelected(0)) || mode.isSelected(1)
     }
+
+    private var prevFallDistance = 0.0f
 
     val eventConsumer = Consumer<Event> { event ->
         when (event) {
             is EventPacket -> {
-                if(event.type == EventPacket.Type.SEND && event.packet is PlayerMoveC2SPacket && mode.isSelected(0)) {
-                    if(mc.player?.fallDistance!! >= fallDistance.value || groundSpoofMode.isSelected(1)) {
+                if (event.type == EventPacket.Type.SEND && event.packet is PlayerMoveC2SPacket && mode.isSelected(0)) {
+                    if (mc.player?.fallDistance!! >= fallDistance.value || groundSpoofMode.isSelected(1)) {
                         (event.packet as IPlayerMoveC2SPacket).tarasande_setOnGround(groundSpoofMode.isSelected(0))
-                        if(resetFallDistance.value)
+                        if (resetFallDistance.value)
                             mc.player?.fallDistance = 0.0f
                     }
                 }
             }
+
             is EventUpdate -> {
-                if(event.state == EventUpdate.State.POST && mode.isSelected(1))
-                    if(mc.player?.fallDistance!! >= fallDistance.value) {
+                if (event.state == EventUpdate.State.POST) {
+                    if (mode.isSelected(1) && mc.player?.fallDistance!! >= fallDistance.value) {
                         mc.networkHandler?.sendPacket(PlayerMoveC2SPacket.OnGroundOnly(true))
-                        if(resetFallDistance.value)
+                        if (resetFallDistance.value)
                             mc.player?.fallDistance = 0.0f
                     }
+                    prevFallDistance = mc.player?.fallDistance!!
+                }
             }
+
             is EventMovement -> {
-                if(event.entity != mc.player)
+                if (event.entity != mc.player)
                     return@Consumer
-                if(mode.isSelected(2))
+                if (mode.isSelected(2))
                     (event.velocity as IVec3d).tarasande_setY(-motion.value)
+            }
+
+            is EventKeyBindingIsPressed -> {
+                if (mode.isSelected(3) && event.keyBinding == mc.options.sneakKey && prevFallDistance > fallDistance.value && mc.options.jumpKey.isPressed) {
+                    event.pressed = !MinecraftClient.getInstance().world!!.isSpaceEmpty(mc.player, mc.player?.boundingBox?.offset(0.0, mc.player?.velocity?.y!!, 0.0))
+                }
             }
         }
     }
-
 }
