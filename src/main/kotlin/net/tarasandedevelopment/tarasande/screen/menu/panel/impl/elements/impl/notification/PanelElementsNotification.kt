0e0185cd855d35ed5043289a9b4baad86f763ec0
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-04 15:55:23 +0200

    removed default text

--- src/main/kotlin/net/tarasandedevelopment/tarasande/screen/menu/panel/impl/elements/impl/notification/PanelElementsNotification.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/screen/menu/panel/impl/elements/impl/notification/PanelElementsNotification.kt
@@ -3,11 +3,5 @@ package net.tarasandedevelopment.tarasande.screen.menu.panel.impl.elements.impl.
 import net.tarasandedevelopment.tarasande.base.screen.menu.valuecomponent.ValueComponent
 import net.tarasandedevelopment.tarasande.screen.menu.ScreenCheatMenu
 import net.tarasandedevelopment.tarasande.screen.menu.panel.impl.elements.PanelElements
-import net.tarasandedevelopment.tarasande.util.player.chat.CommunicationUtil
 
-class PanelElementsNotification(x: Double, y: Double, val screenCheatMenu: ScreenCheatMenu) : PanelElements<ValueComponent>("Notifications", x, y, 150.0, 100.0) {
-
-    init {
-        CommunicationUtil.add("Here you can see all notifications")
-    }
-}
+class PanelElementsNotification(x: Double, y: Double, val screenCheatMenu: ScreenCheatMenu) : PanelElements<ValueComponent>("Notifications", x, y, 150.0, 100.0)
