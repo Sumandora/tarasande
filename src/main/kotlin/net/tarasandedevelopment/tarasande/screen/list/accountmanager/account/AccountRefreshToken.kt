4af83b9e5b241fcf19baec9215d33081d5c5cbf2
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-04 17:34:10 +0200

    added Support for Client Secret

--- src/main/kotlin/net/tarasandedevelopment/tarasande/screen/list/accountmanager/account/AccountRefreshToken.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/screen/list/accountmanager/account/AccountRefreshToken.kt
@@ -9,11 +9,12 @@ class AccountRefreshToken(
     @TextFieldInfo("Redirect-Uri", false) private val _redirectUri: String,
     @TextFieldInfo("Client-Id", true) private val _clientId: String,
     @TextFieldInfo("Refresh-Token", true) private val token: String,
-    @TextFieldInfo("Scope", false) private val _scope: String
+    @TextFieldInfo("Scope", false) private val _scope: String,
+    @TextFieldInfo("Client-Secret", true) private val _clientSecret: String
 ) : AccountMicrosoft() {
 
     @Suppress("unused") // Reflections
-    constructor() : this("", "", "", "")
+    constructor() : this("", "", "", "", "")
 
     override fun setupHttpServer(): ServerSocket {
         error("Account is invalid")
@@ -23,6 +24,9 @@ class AccountRefreshToken(
         clientId = _clientId
         redirectUri = _redirectUri
         scope = _scope
+        if (_clientSecret.isNotEmpty()) {
+            clientSecret = _clientSecret
+        }
         msAuthProfile = buildFromRefreshToken(token)
 
         super.logIn()
@@ -32,5 +36,5 @@ class AccountRefreshToken(
         return if (session == null) "Unnamed Refresh-Token account" else super.getDisplayName()
     }
 
-    override fun create(credentials: List<String>) = AccountRefreshToken(credentials[0], credentials[1], credentials[2], credentials[3])
+    override fun create(credentials: List<String>) = AccountRefreshToken(credentials[0], credentials[1], credentials[2], credentials[3], credentials[4])
 }
