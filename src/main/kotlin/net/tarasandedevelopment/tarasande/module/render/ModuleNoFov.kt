cbc703adfe851d6d32f9f0be3f217de29877ddbd
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-10-05 15:31:55 +0200

    No fov: Rename & more authentic to 1.8

--- src/main/kotlin/net/tarasandedevelopment/tarasande/module/render/ModuleNoFov.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/module/render/ModuleNoFov.kt
@@ -13,11 +13,11 @@ import java.util.function.Consumer
 class ModuleNoFov : Module("No Fov", "Limits the dynamic fov", ModuleCategory.RENDER) {
 
     private val limit = ValueNumberRange(this, "Limit", 0.1, 1.0, 1.15, 1.5, 0.01)
-    private val dontMultiplyWhenIdling = ValueBoolean(this, "Don't multiply when idling", true)
+    private val resetWhenIdling = ValueBoolean(this, "Reset when idling", true)
 
     val eventConsumer = Consumer<Event> { event ->
         if (event is EventMovementFovMultiplier) {
-            if (dontMultiplyWhenIdling.value && !PlayerUtil.isPlayerMoving())
+            if (resetWhenIdling.value && !(PlayerUtil.isPlayerMoving() && mc.player?.isSprinting == true))
                 event.movementFovMultiplier = 1.0f
             else
                 event.movementFovMultiplier = MathHelper.clamp(event.movementFovMultiplier, limit.minValue.toFloat(), limit.maxValue.toFloat())
