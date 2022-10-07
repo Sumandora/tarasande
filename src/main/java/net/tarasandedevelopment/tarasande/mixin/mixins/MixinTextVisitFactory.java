d917bddea7c66a9d10f71acc98592b99eac3ca24
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-07 16:53:13 +0200

    Event: fixed TextVisit

--- src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/MixinTextVisitFactory.java
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/MixinTextVisitFactory.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(TextVisitFactory.class)
 public class MixinTextVisitFactory {
 
-    @ModifyVariable(method = "visitFormatted(Ljava/lang/String;Lnet/minecraft/text/Style;Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("LOAD"), argsOnly = true, ordinal = 0)
+    @ModifyVariable(method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("LOAD"), argsOnly = true, ordinal = 0)
     private static String modifyText(String value) {
         EventTextVisit eventTextVisit = new EventTextVisit(value);
         TarasandeMain.Companion.get().getManagerEvent().call(eventTextVisit);
