7d496eafc67da0ff449bf437e5e6dec719fcef7c
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-11 22:57:10 +0200

    Protocol Hack: fixed tick increment check

--- src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/entity/MixinClientPlayerEntity.java
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/entity/MixinClientPlayerEntity.java
@@ -2,18 +2,23 @@ package net.tarasandedevelopment.tarasande.mixin.mixins.protocolhack.entity;
 
 import com.mojang.authlib.GameProfile;
 import de.florianmichael.viaprotocolhack.util.VersionList;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.encryption.PlayerPublicKey;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.tarasandedevelopment.tarasande.TarasandeMain;
 import net.tarasandedevelopment.tarasande.mixin.accessor.protocolhack.IClientPlayerEntity_Protocol;
 import net.tarasandedevelopment.tarasande.module.movement.ModuleNoSlowdown;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -23,14 +28,27 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = ClientPlayerEntity.class, priority = 2000)
-public class MixinClientPlayerEntity extends AbstractClientPlayerEntity implements IClientPlayerEntity_Protocol {
+public abstract class MixinClientPlayerEntity extends AbstractClientPlayerEntity implements IClientPlayerEntity_Protocol {
 
     @Shadow private boolean lastOnGround;
 
     @Shadow public Input input;
 
+    @Shadow private int ticksSinceLastPositionPacketSent;
+
+    @Shadow protected abstract boolean isCamera();
+
+    @Shadow private double lastX;
+    @Shadow private double lastBaseY;
+    @Shadow private double lastZ;
+    @Shadow private float lastYaw;
+    @Shadow private float lastPitch;
+    @Shadow @Final public ClientPlayNetworkHandler networkHandler;
+    @Shadow private boolean autoJumpEnabled;
+    @Shadow @Final protected MinecraftClient client;
     @Unique
     private boolean areSwingCanceledThisTick = false;
 
@@ -49,20 +67,52 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity implemen
         super.tickMovement();
     }
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "FIELD", target = "Lnet/minecraft/client/network/ClientPlayerEntity;lastOnGround:Z", opcode = Opcodes.GETFIELD))
-    public boolean redirectSendMovementPackets(ClientPlayerEntity player) {
-        if (VersionList.isOlderOrEqualTo(VersionList.R1_8))
-            return !this.lastOnGround; // make sure player packets are sent every tick to tick the server-side player entity
-        else
-            return this.lastOnGround;
-    }
+    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isCamera()Z"))
+    public boolean fixMovement(ClientPlayerEntity instance) {
+        if (this.isCamera()) {
+            boolean bl4;
+            double d = this.getX() - this.lastX;
+            double e = this.getY() - this.lastBaseY;
+            double f = this.getZ() - this.lastZ;
+            double g = this.getYaw() - this.lastYaw;
+            double h = this.getPitch() - this.lastPitch;
+            if (VersionList.isNewerTo(VersionList.R1_8))
+                ++this.ticksSinceLastPositionPacketSent;
+            double n = 2.05E-4;
+            if (VersionList.isOlderOrEqualTo(VersionList.R1_8))
+                n = 9.0E-4D;
+            boolean bl3 = MathHelper.squaredMagnitude(d, e, f) > MathHelper.square(n) || this.ticksSinceLastPositionPacketSent >= 20;
+            bl4 = g != 0.0 || h != 0.0;
+            if (this.hasVehicle()) {
+                Vec3d vec3d = this.getVelocity();
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(vec3d.x, -999.0, vec3d.z, this.getYaw(), this.getPitch(), this.onGround));
+                bl3 = false;
+            } else if (bl3 && bl4) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(this.getX(), this.getY(), this.getZ(), this.getYaw(), this.getPitch(), this.onGround));
+            } else if (bl3) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(this.getX(), this.getY(), this.getZ(), this.onGround));
+            } else if (bl4) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(this.getYaw(), this.getPitch(), this.onGround));
+            } else if (this.lastOnGround != this.onGround || VersionList.isOlderOrEqualTo(VersionList.R1_8)) {
+                this.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(this.onGround));
+            }
+            if (VersionList.isOlderOrEqualTo(VersionList.R1_8))
+                ++this.ticksSinceLastPositionPacketSent;
 
-    @Redirect(method = "sendMovementPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;square(D)D"))
-    public double redirectSendMovementPackets_2(double n) {
-        if (VersionList.isOlderOrEqualTo(VersionList.R1_8))
-            n = 9.0E-4D;
-
-        return MathHelper.square(n);
+            if (bl3) {
+                this.lastX = this.getX();
+                this.lastBaseY = this.getY();
+                this.lastZ = this.getZ();
+                this.ticksSinceLastPositionPacketSent = 0;
+            }
+            if (bl4) {
+                this.lastYaw = this.getYaw();
+                this.lastPitch = this.getPitch();
+            }
+            this.lastOnGround = this.onGround;
+            this.autoJumpEnabled = this.client.options.getAutoJump().getValue();
+        }
+        return false;
     }
 
     @Inject(method = "swingHand", at = @At("HEAD"), cancellable = true)
