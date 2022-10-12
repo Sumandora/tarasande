07b0f54a2a88ebda7815f72bfee33b7d5e02fc8a
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-12 18:12:22 +0200

    Module: add moon phase support to WorldTime module

--- src/main/kotlin/net/tarasandedevelopment/tarasande/module/render/ModuleWorldTime.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/module/render/ModuleWorldTime.kt
@@ -8,13 +8,28 @@ import net.tarasandedevelopment.tarasande.base.module.ModuleCategory
 import net.tarasandedevelopment.tarasande.event.EventPacket
 import net.tarasandedevelopment.tarasande.event.EventUpdate
 import net.tarasandedevelopment.tarasande.mixin.accessor.IWorldTimeUpdateS2CPacket
+import net.tarasandedevelopment.tarasande.value.ValueBoolean
 import net.tarasandedevelopment.tarasande.value.ValueNumber
 import java.util.function.Consumer
 
 class ModuleWorldTime : Module("World time", "Changes the time of day", ModuleCategory.RENDER) {
 
+    private val modifyTime = ValueBoolean(this, "Modify time", true)
     // I am unsure if this is supposed to be the max time or is just a coincidence to be the same value
-    private val time = ValueNumber(this, "Time", 0.0, World.field_30969 / 2.0, World.field_30969.toDouble(), 1.0)
+    private val time = object : ValueNumber(this, "Time", 0.0, World.field_30969 / 2.0, World.field_30969.toDouble(), 1.0) {
+        override fun isEnabled() = modifyTime.value
+    }
+
+    private val modifyMoonPhase = ValueBoolean(this, "Modify moon phase", false)
+    private val moonPhase = object : ValueNumber(this, "Moon phase", 0.0, 0.0, 7.0, 1.0) {
+        override fun isEnabled() = modifyMoonPhase.value
+    }
+
+    fun moonPhase(): Int {
+        if (!enabled || !modifyMoonPhase.value) return -1
+
+        return moonPhase.value.toInt()
+    }
 
     val eventConsumer = Consumer<Event> { event ->
         when (event) {
@@ -31,5 +46,4 @@ class ModuleWorldTime : Module("World time", "Changes the time of day", ModuleCa
             }
         }
     }
-
 }
