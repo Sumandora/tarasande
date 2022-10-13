6722357ae6c87470bbda032228d4feb8cae9d5e6
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-13 19:33:03 +0200

    Module: WorldTime -> replace moon phase value with mode names Module: PerfectHorseJump -> Fix crash

--- src/main/kotlin/net/tarasandedevelopment/tarasande/module/movement/ModulePerfectHorseJump.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/module/movement/ModulePerfectHorseJump.kt
@@ -4,7 +4,7 @@ import net.minecraft.client.MinecraftClient
 import net.tarasandedevelopment.tarasande.base.event.Event
 import net.tarasandedevelopment.tarasande.base.module.Module
 import net.tarasandedevelopment.tarasande.base.module.ModuleCategory
-import net.tarasandedevelopment.tarasande.event.EventTick
+import net.tarasandedevelopment.tarasande.event.EventUpdate
 import net.tarasandedevelopment.tarasande.mixin.accessor.IClientPlayerEntity
 import net.tarasandedevelopment.tarasande.value.ValueNumber
 import java.util.function.Consumer
@@ -15,7 +15,7 @@ class ModulePerfectHorseJump : Module("Perfect horse jump", "Forces perfect hors
     private val jumpPowerCounter = ValueNumber(this, "Jump power: counter", 1.0, 9.0, 9.0, 1E-2)
 
     val eventConsumer = Consumer<Event> {
-        if (it is EventTick) {
+        if (it is EventUpdate && it.state == EventUpdate.State.PRE) {
             (MinecraftClient.getInstance().player as IClientPlayerEntity).tarasande_setMountJumpStrength(this.jumpPower.value.toFloat())
             (MinecraftClient.getInstance().player as IClientPlayerEntity).tarasande_setField_3938(this.jumpPowerCounter.value.toInt())
         }
