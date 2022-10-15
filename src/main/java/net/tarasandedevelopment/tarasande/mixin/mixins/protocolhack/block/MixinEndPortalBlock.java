eb808633156a57e5057077170f89e91b9dc810c1
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-15 16:52:45 +0200

    Protocol Hack: fixed cache crash

--- src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/block/MixinEndPortalBlock.java
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/block/MixinEndPortalBlock.java
@@ -1,28 +1,32 @@
 package net.tarasandedevelopment.tarasande.mixin.mixins.protocolhack.block;
 
 import de.florianmichael.viaprotocolhack.util.VersionList;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.EndPortalBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(EndPortalBlock.class)
-public class MixinEndPortalBlock {
+public abstract class MixinEndPortalBlock extends BlockWithEntity {
 
+    @Shadow @Final protected static VoxelShape SHAPE;
     @Unique
     private final VoxelShape SHAPE_1_8 = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
 
     @Unique
     private final VoxelShape SHAPE_1_16_5 = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 12.0, 16.0);
+
+    protected MixinEndPortalBlock(Settings settings) {
+        super(settings);
+    }
 
     @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
     public void injectGetOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
@@ -32,5 +36,10 @@ public class MixinEndPortalBlock {
             cir.setReturnValue(SHAPE_1_8);
         else if (VersionList.isOlderOrEqualTo(VersionList.R1_16_5))
             cir.setReturnValue(SHAPE_1_16_5);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 }
