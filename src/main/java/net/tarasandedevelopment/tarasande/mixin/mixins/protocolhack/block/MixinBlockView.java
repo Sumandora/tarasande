c6286e247a675e5735de0b7835a7a9d7b752fb32
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-10-15 16:53:48 +0200

    Prevent getRaycastShape = null crash

---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/block/MixinBlockView.java
@@ -0,0 +1,24 @@
+package net.tarasandedevelopment.tarasande.mixin.mixins.protocolhack.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(BlockView.class)
+public class MixinBlockView {
+
+    @Redirect(method = "raycastBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getRaycastShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;"))
+    public VoxelShape hookedGetRaycastShape(BlockState instance, BlockView blockView, BlockPos blockPos) {
+        VoxelShape shape = instance.getRaycastShape(blockView, blockPos);
+        // It appears, that certain game states react unstable to the shape changes we are producing.
+        if (shape == null)
+            shape = VoxelShapes.empty();
+        return shape;
+    }
+
+}
