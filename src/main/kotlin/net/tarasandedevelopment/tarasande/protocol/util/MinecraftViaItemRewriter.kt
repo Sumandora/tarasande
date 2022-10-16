6c41f5c567269d944baa615c496fb72c90b11b53
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-16 21:25:22 +0200

    better class name

--- src/main/kotlin/net/tarasandedevelopment/tarasande/protocol/util/MinecraftViaItemRewriter.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/protocol/util/MinecraftViaItemRewriter.kt
@@ -3,7 +3,6 @@ package net.tarasandedevelopment.tarasande.protocol.util
 import com.viaversion.viaversion.api.Via
 import com.viaversion.viaversion.api.minecraft.item.DataItem
 import com.viaversion.viaversion.api.minecraft.item.Item
-import com.viaversion.viaversion.api.protocol.Protocol
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.ByteTag
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.CompoundTag
 import com.viaversion.viaversion.libs.opennbt.tag.builtin.DoubleTag
@@ -39,7 +38,7 @@ import net.tarasandedevelopment.tarasande.mixin.accessor.protocolhack.IProtocolM
 
 object MinecraftViaItemRewriter {
 
-    private val itemMappings = ArrayList<Pair<Int, RemappBypass>>()
+    private val itemMappings = ArrayList<Pair<Int, ViaItemRewriterImpl>>()
 
     fun minecraftToViaItem(stack: ItemStack) = minecraftToViaItem(stack, SharedConstants.getProtocolVersion())
 
@@ -69,27 +68,27 @@ object MinecraftViaItemRewriter {
         return via
     }
 
-    private fun allItemMappings(): List<Pair<Int, RemappBypass>> {
-        val list = ArrayList<Pair<Int, RemappBypass>>()
+    private fun allItemMappings(): List<Pair<Int, ViaItemRewriterImpl>> {
+        val list = ArrayList<Pair<Int, ViaItemRewriterImpl>>()
         (Via.getManager().protocolManager as IProtocolManagerImpl_Protocol).protocols.filter { p -> p.second.itemRewriter != null }.forEach {
-            list.add(Pair(it.first, object : RemappBypass {
+            list.add(Pair(it.first, object : ViaItemRewriterImpl {
                 override fun remapItem(dataItem: Item): Item? {
                     return it.second.itemRewriter!!.handleItemToServer(dataItem)
                 }
             }))
         }
-        list.add(Pair(VersionList.R1_9_4.version, object : RemappBypass {
+        list.add(Pair(VersionList.R1_9_4.version, object : ViaItemRewriterImpl {
             override fun remapItem(dataItem: Item): Item? {
                 return Via.getManager().protocolManager.getProtocol(Protocol1_10To1_9_3_4::class.java)!!.itemRewriter!!.handleItemToServer(dataItem)
             }
         }))
-        list.add(Pair(VersionList.R1_8.version, object : RemappBypass {
+        list.add(Pair(VersionList.R1_8.version, object : ViaItemRewriterImpl {
             override fun remapItem(dataItem: Item): Item {
                 com.viaversion.viaversion.protocols.protocol1_9to1_8.ItemRewriter.toServer(dataItem)
                 return dataItem
             }
         }))
-        list.add(Pair(LegacyProtocolVersion.R1_7_10.version, object : RemappBypass {
+        list.add(Pair(LegacyProtocolVersion.R1_7_10.version, object : ViaItemRewriterImpl {
             override fun remapItem(dataItem: Item): Item {
                 ItemRewriter.toServer(dataItem)
                 return dataItem
@@ -98,7 +97,7 @@ object MinecraftViaItemRewriter {
         return list
     }
 
-    interface RemappBypass {
+    interface ViaItemRewriterImpl {
         fun remapItem(dataItem: Item): Item?
     }
 
