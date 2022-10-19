cc39e05ad08a3b7b58a88662b7a51d1bb155452d
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-10-19 12:50:59 +0200

    Inventory Cleaner now drops lower material items if no enchantments are being lost

---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/accessor/IArmorMaterials.java
@@ -0,0 +1,5 @@
+package net.tarasandedevelopment.tarasande.mixin.accessor;
+
+public interface IArmorMaterials {
+    int tarasande_getDurabilityMultiplier();
+}
--- src/main/kotlin/net/tarasandedevelopment/tarasande/util/player/container/ContainerUtil.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/util/player/container/ContainerUtil.kt
@@ -5,6 +5,7 @@ import net.minecraft.item.*
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.slot.Slot
 import net.minecraft.util.math.Vec2f
+import net.tarasandedevelopment.tarasande.mixin.accessor.IArmorMaterials
 import net.tarasandedevelopment.tarasande.mixin.accessor.IHandledScreen
 
 object ContainerUtil {
@@ -25,17 +26,38 @@ object ContainerUtil {
             .minByOrNull { lastMouseClick.distanceSquared(getDisplayPosition(accessor, it)) }
     }
 
+    private fun wrapMaterialScore(stack: ItemStack): Float? {
+        return when (stack.item) {
+            is SwordItem -> (stack.item as ToolItem).material.attackDamage
+            is ToolItem -> (stack.item as ToolItem).material.durability.toFloat()
+            is ArmorItem -> ((stack.item as ArmorItem).material as IArmorMaterials).tarasande_getDurabilityMultiplier().toFloat()
+            else -> null
+        }
+    }
+
+    private fun isSameItemType(stack: ItemStack, otherStack: ItemStack): Boolean {
+        return when {
+            stack.item is ToolItem && otherStack.item is ToolItem -> true
+            stack.item is ArmorItem && otherStack.item is ArmorItem -> true
+            else -> stack.item == otherStack.item
+        }
+    }
+
     fun hasBetterEquivalent(stack: ItemStack, list: List<ItemStack>): Boolean {
-        if (stack.item !is SwordItem && stack.item !is ToolItem && stack.item !is ArmorItem && stack.item !is FishingRodItem && stack.item !is BowItem)
+        if (stack.item !is ToolItem && stack.item !is ArmorItem && stack.item !is FishingRodItem && stack.item !is BowItem)
             return false
+
+        val materialScore = wrapMaterialScore(stack)
 
         val enchantments = EnchantmentHelper.get(stack)
 
         for (otherStack in list) {
-            if (stack.item == otherStack.item) {
+            if (isSameItemType(stack, otherStack)) {
                 val otherEnchantments = EnchantmentHelper.get(otherStack)
 
-                if (enchantments.all { otherEnchantments.containsKey(it.key) && enchantments[it.key]!! >= it.value })
+                val otherMaterialScore = wrapMaterialScore(otherStack)
+
+                if (((materialScore == null || otherMaterialScore == null) || otherMaterialScore > materialScore) && enchantments.all { otherEnchantments.containsKey(it.key) && otherEnchantments[it.key]!! >= it.value })
                     return true
             }
         }
