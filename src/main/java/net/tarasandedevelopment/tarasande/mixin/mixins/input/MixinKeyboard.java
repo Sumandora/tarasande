74630a84832fdc21b3dd49b921075b99cb2eb145
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-20 16:16:37 +0200

    Protocol Hack: post check fix

---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/input/MixinKeyboard.java
@@ -0,0 +1,23 @@
+package net.tarasandedevelopment.tarasande.mixin.mixins.input;
+
+import de.florianmichael.viaprotocolhack.util.VersionList;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import net.tarasandedevelopment.tarasande.protocol.util.InputTracker1_12_2;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Keyboard.class)
+public class MixinKeyboard {
+
+    @Redirect(method = "*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;execute(Ljava/lang/Runnable;)V"))
+    public void redirectSync(MinecraftClient instance, Runnable runnable) {
+        if (VersionList.isOlderOrEqualTo(VersionList.R1_12_2)) {
+            InputTracker1_12_2.INSTANCE.getKeyboard().add(runnable);
+            return;
+        }
+
+        instance.execute(runnable);
+    }
+}
---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/input/MixinMouse.java
@@ -0,0 +1,23 @@
+package net.tarasandedevelopment.tarasande.mixin.mixins.input;
+
+import de.florianmichael.viaprotocolhack.util.VersionList;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
+import net.tarasandedevelopment.tarasande.protocol.util.InputTracker1_12_2;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Mouse.class)
+public class MixinMouse {
+
+    @Redirect(method = { "method_29615", "method_22685", "method_22684" }, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;execute(Ljava/lang/Runnable;)V"))
+    public void redirectSync(MinecraftClient instance, Runnable runnable) {
+        if (VersionList.isOlderOrEqualTo(VersionList.R1_12_2)) {
+            InputTracker1_12_2.INSTANCE.getMouse().add(runnable);
+            return;
+        }
+
+        instance.execute(runnable);
+    }
+}
