41526605c0d40772f0810a0bf7d97a5af870cee0
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-23 03:59:26 +0200

    removed unused imports

--- src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/core/forgefaker/MixinMultiplayerServerListWidgetSubServerEntry.java
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/core/forgefaker/MixinMultiplayerServerListWidgetSubServerEntry.java
@@ -1,27 +1,16 @@
 package net.tarasandedevelopment.tarasande.mixin.mixins.core.forgefaker;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Text;
 import net.tarasandedevelopment.tarasande.TarasandeMain;
 import net.tarasandedevelopment.tarasande.event.EventRenderMultiplayerEntry;
-import net.tarasandedevelopment.tarasande.mixin.accessor.forgefaker.IServerInfo;
-import net.tarasandedevelopment.tarasande.screen.clientmenu.forgefaker.payload.IForgePayload;
-import net.tarasandedevelopment.tarasande.screen.clientmenu.forgefaker.payload.modern.ModernForgePayload;
-import net.tarasandedevelopment.tarasande.screen.clientmenu.forgefaker.ui.ScreenBetterForgeModList;
-import net.tarasandedevelopment.tarasande.util.render.RenderUtil;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mixin(MultiplayerServerListWidget.ServerEntry.class)
 public class MixinMultiplayerServerListWidgetSubServerEntry {
