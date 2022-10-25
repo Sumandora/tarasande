ab68d5a1588ba6bc5e52631b92eee87d38d822b1
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-25 22:25:21 +0200

    client-value to module

---
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/module/misc/ModuleAllowEveryCharacter.kt
@@ -0,0 +1,6 @@
+package net.tarasandedevelopment.tarasande.module.misc
+
+import net.tarasandedevelopment.tarasande.base.module.Module
+import net.tarasandedevelopment.tarasande.base.module.ModuleCategory
+
+class ModuleAllowEveryCharacter : Module("Allow every character", "Allows all characters in chat", ModuleCategory.MISC)
