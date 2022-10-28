e6a9a58529d6f65f9cb9dbeeabd41adc82ecb68a
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-28 16:58:38 +0200

    Protocol Hack: MultiBlockChange in 1.7.10

--- src/main/java/de/florianmichael/vialegacy/protocols/protocol1_8to1_7_10/Protocol1_8to1_7_10.java
+++ src/main/java/de/florianmichael/vialegacy/protocols/protocol1_8to1_7_10/Protocol1_8to1_7_10.java
@@ -15,6 +15,8 @@
 package de.florianmichael.vialegacy.protocols.protocol1_8to1_7_10;
 
 import com.viaversion.viaversion.api.connection.UserConnection;
+import com.viaversion.viaversion.api.minecraft.BlockChangeRecord;
+import com.viaversion.viaversion.api.minecraft.BlockChangeRecord1_8;
 import com.viaversion.viaversion.api.minecraft.Position;
 import com.viaversion.viaversion.api.minecraft.entities.Entity1_10Types;
 import com.viaversion.viaversion.api.minecraft.item.DataItem;
@@ -57,6 +59,8 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
@@ -213,8 +217,119 @@ public class Protocol1_8to1_7_10 extends EnZaProtocol<ClientboundPackets1_7_10, 
 
             @Override
             public void registerMap() {
+                handler(pw -> {
+                    short columnCount = pw.read(Type.SHORT); // short1
+                    int size = pw.read(Type.INT); // size
+                    boolean skyLightSent = pw.read(Type.BOOLEAN); // h
+                    int[] chunkX = new int[columnCount]; // a
+                    int[] chunkZ = new int[columnCount]; // b
+                    int[] primaryBitMask = new int[columnCount]; // c
+                    int[] addBitMask = new int[columnCount]; // d
+                    byte[][] inflatedBuffers = new byte[columnCount][]; // inflatedBuffers
+                    CustomByteType customByteType = new CustomByteType(size);
+                    byte[] buildBuffer = pw.read(customByteType); // buildBuffer
+
+                    byte[] data = new byte[196864 * columnCount]; // abyte
+                    Inflater inflater = new Inflater();
+                    inflater.setInput(buildBuffer, 0, size);
+
+                    try {
+                        inflater.inflate(data);
+                    } catch (DataFormatException ex) {
+                        throw new IOException("Bad compressed data format");
+                    } finally {
+                        inflater.end();
+                    }
+
+                    int i = 0;
+
+                    for (int j = 0; j < columnCount; ++j) {
+                        chunkX[j] = pw.read(Type.INT);
+                        chunkZ[j] = pw.read(Type.INT);
+                        primaryBitMask[j] = pw.read(Type.SHORT);
+                        addBitMask[j] = pw.read(Type.SHORT);
+                        int k = 0;
+                        int l = 0;
+
+                        int i1;
+                        for (i1 = 0; i1 < 16; ++i1) {
+                            k += primaryBitMask[j] >> i1 & 1;
+                            l += addBitMask[j] >> i1 & 1;
+                        }
+
+                        i1 = 8192 * k + 256;
+                        i1 += 2048 * l;
+                        if (skyLightSent) {
+                            i1 += 2048 * k;
+                        }
+
+                        inflatedBuffers[j] = new byte[i1];
+                        System.arraycopy(data, i, inflatedBuffers[j], 0, i1);
+                        i += i1;
+                    }
+
+                    Chunk1_8to1_7_6_10[] chunks = new Chunk1_8to1_7_6_10[columnCount];
+                    for (i = 0; i < columnCount; i++) {
+                        chunks[i] = new Chunk1_8to1_7_6_10(inflatedBuffers[i], primaryBitMask[i], addBitMask[i], skyLightSent,
+                                true);
+                    }
+
+                    pw.write(Type.BOOLEAN, skyLightSent);
+                    pw.write(Type.VAR_INT, (int) columnCount);
+
+                    for (i = 0; i < columnCount; ++i) {
+                        pw.write(Type.INT, chunkX[i]);
+                        pw.write(Type.INT, chunkZ[i]);
+                        pw.write(Type.UNSIGNED_SHORT, primaryBitMask[i]);
+                    }
+
+                    for (i = 0; i < columnCount; ++i) {
+                        data = chunks[i].get1_8Data();
+                        customByteType = new CustomByteType(data.length);
+                        pw.write(customByteType, data);
+                    }
+                    ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+                    try {
+                        pw.writeToBuffer(buffer);
+                        Type.VAR_INT.readPrimitive(buffer); // Remove Packet ID
+                        pw.clearPacket();
+
+                        ((PacketWrapperImpl) pw).getInputBuffer().writeBytes(buffer);
+                    } catch (Exception e) {
+                        buffer.release();
+                    }
+                });
+            }
+        });
+
+        this.registerClientbound(ClientboundPackets1_7_10.MULTI_BLOCK_CHANGE, new PacketRemapper() {
+            @Override
+            public void registerMap() {
                 handler((pw) -> {
-                    transformChunkBulk(pw);
+                    final int chunkX = pw.read(Type.INT);
+                    final int chunkZ = pw.read(Type.INT);
+                    final int size = pw.read(Type.SHORT);
+
+                    pw.read(Type.INT);
+                    final short[] blocks = new short[size];
+                    final short[] positions = new short[size];
+
+                    for (int i = 0; i < size; i++) {
+                        positions[i] = pw.read(Type.SHORT);
+                        blocks[i] = pw.read(Type.SHORT);
+                    }
+
+                    pw.write(Type.INT, chunkX);
+                    pw.write(Type.INT, chunkZ);
+                    pw.write(Type.BLOCK_CHANGE_RECORD_ARRAY, IntStream.of(0, size).mapToObj(value -> {
+                        final short encodedPos = positions[value];
+
+                        final int x = encodedPos >>> 12 & 0xF;
+                        final int y = encodedPos & 0xFF;
+                        final int z = encodedPos >>> 8 & 0xF;
+
+                        return new BlockChangeRecord1_8(x, y, z, blocks[value]);
+                    }).toList().toArray(BlockChangeRecord[]::new));
                 });
             }
         });
@@ -1408,146 +1523,6 @@ public class Protocol1_8to1_7_10 extends EnZaProtocol<ClientboundPackets1_7_10, 
                 });
             }
         });
-    }
-
-    public void transformChunk(PacketWrapper packetWrapper) throws Exception {
-        int chunkX = packetWrapper.read(Type.INT);
-        int chunkZ = packetWrapper.read(Type.INT);
-        boolean groundUp = packetWrapper.read(Type.BOOLEAN);
-        int primaryBitMask = packetWrapper.read(Type.SHORT);
-        int addBitMask = packetWrapper.read(Type.SHORT);
-        int compressedSize = packetWrapper.read(Type.INT);
-        CustomByteType customByteType = new CustomByteType(compressedSize);
-        byte[] data = packetWrapper.read(customByteType);
-
-        int k = 0;
-        int l = 0;
-
-        for (int j = 0; j < 16; ++j) {
-            k += primaryBitMask >> j & 1;
-            l += addBitMask >> j & 1;
-        }
-
-        int uncompressedSize = 12288 * k;
-        uncompressedSize += 2048 * l;
-        if (groundUp) {
-            uncompressedSize += 256;
-        }
-
-        byte[] uncompressedData = new byte[uncompressedSize];
-        Inflater inflater = new Inflater();
-        inflater.setInput(data, 0, compressedSize);
-        try {
-            inflater.inflate(uncompressedData);
-        } catch (DataFormatException ex) {
-            throw new IOException("Bad compressed data format");
-        } finally {
-            inflater.end();
-        }
-
-        Chunk1_8to1_7_6_10 chunk = new Chunk1_8to1_7_6_10(uncompressedData, primaryBitMask, addBitMask, true, groundUp);
-
-        Field field = PacketWrapperImpl.class.getDeclaredField("packetValues");
-        field.setAccessible(true);
-        ((List) field.get(packetWrapper)).clear();
-        field = PacketWrapperImpl.class.getDeclaredField("readableObjects");
-        field.setAccessible(true);
-        ((ArrayDeque) field.get(packetWrapper)).clear();
-        field = PacketWrapperImpl.class.getDeclaredField("inputBuffer");
-        field.setAccessible(true);
-        ByteBuf buffer = (ByteBuf) field.get(packetWrapper);
-        buffer.clear();
-
-        buffer.writeInt(chunkX);
-        buffer.writeInt(chunkZ);
-        buffer.writeBoolean(groundUp);
-        buffer.writeShort(primaryBitMask);
-        byte[] finaldata = chunk.get1_8Data();
-        Type.VAR_INT.writePrimitive(buffer, finaldata.length);
-        buffer.writeBytes(finaldata);
-    }
-
-    public void transformChunkBulk(PacketWrapper packetWrapper) throws Exception {
-        short columnCount = packetWrapper.read(Type.SHORT); // short1
-        int size = packetWrapper.read(Type.INT); // size
-        boolean skyLightSent = packetWrapper.read(Type.BOOLEAN); // h
-        int[] chunkX = new int[columnCount]; // a
-        int[] chunkZ = new int[columnCount]; // b
-        int[] primaryBitMask = new int[columnCount]; // c
-        int[] addBitMask = new int[columnCount]; // d
-        byte[][] inflatedBuffers = new byte[columnCount][]; // inflatedBuffers
-        CustomByteType customByteType = new CustomByteType(size);
-        byte[] buildBuffer = packetWrapper.read(customByteType); // buildBuffer
-
-        byte[] data = new byte[196864 * columnCount]; // abyte
-        Inflater inflater = new Inflater();
-        inflater.setInput(buildBuffer, 0, size);
-
-        try {
-            inflater.inflate(data);
-        } catch (DataFormatException ex) {
-            throw new IOException("Bad compressed data format");
-        } finally {
-            inflater.end();
-        }
-
-        int i = 0;
-
-        for (int j = 0; j < columnCount; ++j) {
-            chunkX[j] = packetWrapper.read(Type.INT);
-            chunkZ[j] = packetWrapper.read(Type.INT);
-            primaryBitMask[j] = packetWrapper.read(Type.SHORT);
-            addBitMask[j] = packetWrapper.read(Type.SHORT);
-            int k = 0;
-            int l = 0;
-
-            int i1;
-            for (i1 = 0; i1 < 16; ++i1) {
-                k += primaryBitMask[j] >> i1 & 1;
-                l += addBitMask[j] >> i1 & 1;
-            }
-
-            i1 = 8192 * k + 256;
-            i1 += 2048 * l;
-            if (skyLightSent) {
-                i1 += 2048 * k;
-            }
-
-            inflatedBuffers[j] = new byte[i1];
-            System.arraycopy(data, i, inflatedBuffers[j], 0, i1);
-            i += i1;
-        }
-
-        Chunk1_8to1_7_6_10[] chunks = new Chunk1_8to1_7_6_10[columnCount];
-        for (i = 0; i < columnCount; i++) {
-            chunks[i] = new Chunk1_8to1_7_6_10(inflatedBuffers[i], primaryBitMask[i], addBitMask[i], skyLightSent,
-                    true);
-        }
-
-        packetWrapper.write(Type.BOOLEAN, skyLightSent);
-        packetWrapper.write(Type.VAR_INT, (int) columnCount);
-
-        for (i = 0; i < columnCount; ++i) {
-            packetWrapper.write(Type.INT, chunkX[i]);
-            packetWrapper.write(Type.INT, chunkZ[i]);
-            packetWrapper.write(Type.UNSIGNED_SHORT, primaryBitMask[i]);
-        }
-
-        for (i = 0; i < columnCount; ++i) {
-            data = chunks[i].get1_8Data();
-            customByteType = new CustomByteType(data.length);
-            packetWrapper.write(customByteType, data);
-        }
-        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
-        try {
-            packetWrapper.writeToBuffer(buffer);
-            Type.VAR_INT.readPrimitive(buffer); // Remove Packet ID
-            packetWrapper.clearPacket();
-
-            ((PacketWrapperImpl) packetWrapper).getInputBuffer().writeBytes(buffer);
-        } catch (Exception e) {
-            buffer.release();
-        }
     }
 
     private WindowIDTracker windowTracker(final UserConnection connection) {
