d998bfc499583620d4dea7d6ba42027b986bcf7f
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-10-30 18:16:35 +0100

    Fix ArmorUpdater

--- src/main/kotlin/net/tarasandedevelopment/tarasande/protocol/util/ArmorUpdater1_8_0.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/protocol/util/ArmorUpdater1_8_0.kt
@@ -5,10 +5,10 @@ import com.viaversion.viaversion.api.type.Type
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.ArmorType
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.ClientboundPackets1_9
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.Protocol1_9To1_8
-import de.florianmichael.viaprotocolhack.util.VersionList
 import net.minecraft.client.MinecraftClient
 import net.minecraft.item.Item
 import net.minecraft.item.Items
+import net.minecraft.util.registry.Registry
 import net.tarasandedevelopment.tarasande.mixin.accessor.protocolhack.IClientConnection_Protocol
 import java.util.*
 
@@ -17,7 +17,7 @@ object ArmorUpdater1_8_0 {
     // this is a list of all 1.9 armor items with their id
     private val ids = HashMap<Item, Int>()
 
-    private var oldArmor = 0.0
+    private var oldArmor = 0
 
     init {
         //@formatter:off
@@ -26,22 +26,14 @@ object ArmorUpdater1_8_0 {
             Items.LEATHER_CHESTPLATE,   Items.CHAINMAIL_CHESTPLATE,     Items.IRON_CHESTPLATE,      Items.DIAMOND_CHESTPLATE,   Items.GOLDEN_CHESTPLATE,
             Items.LEATHER_LEGGINGS,     Items.CHAINMAIL_LEGGINGS,       Items.IRON_LEGGINGS,        Items.DIAMOND_LEGGINGS,     Items.GOLDEN_LEGGINGS,
             Items.LEATHER_BOOTS,        Items.CHAINMAIL_BOOTS,          Items.IRON_BOOTS,           Items.DIAMOND_BOOTS,        Items.GOLDEN_BOOTS
-        ).forEach { item ->
-            MinecraftViaItemRewriter.minecraftToViaItem(item.defaultStack, VersionList.R1_8.version)?.also {
-                ids[item] = it.identifier()
-            }
+        ).forEach {
+            ids[it] = ArmorType.findByType(Registry.ITEM.getId(it).toString()).armorPoints
         }
         //@formatter:on
     }
 
     fun update() {
-        var armor = 0.0
-
-        MinecraftClient.getInstance().player!!.inventory.armor.forEach {
-            ids[it.item]?.also { id ->
-                armor += ArmorType.findById(id).armorPoints
-            }
-        }
+        val armor = MinecraftClient.getInstance().player!!.inventory.armor.sumOf { ids[it.item] ?: 0 }
 
         if (armor == oldArmor) return
         oldArmor = armor
@@ -54,7 +46,7 @@ object ArmorUpdater1_8_0 {
         entityPropertiesPacket.write(Type.DOUBLE, 0.0)
         entityPropertiesPacket.write(Type.VAR_INT, 1)
         entityPropertiesPacket.write(Type.UUID, UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150"))
-        entityPropertiesPacket.write(Type.DOUBLE, armor)
+        entityPropertiesPacket.write(Type.DOUBLE, armor.toDouble())
         entityPropertiesPacket.write(Type.BYTE, 0.toByte())
 
         try {
