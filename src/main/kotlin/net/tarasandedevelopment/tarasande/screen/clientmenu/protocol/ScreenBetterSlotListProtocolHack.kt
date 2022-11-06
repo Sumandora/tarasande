0f4b7cfe498ddbc2ea3b07957563197e1d082235
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-06 05:22:50 +0100

    updated VPH

--- src/main/kotlin/net/tarasandedevelopment/tarasande/screen/clientmenu/protocol/ScreenBetterSlotListProtocolHack.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/screen/clientmenu/protocol/ScreenBetterSlotListProtocolHack.kt
@@ -14,12 +14,21 @@ import java.awt.Color
 
 class ScreenBetterSlotListProtocolHack : ScreenBetterSlotList(46, 12) {
 
+    private val specialNames = HashMap<ProtocolVersion, String>()
+
     init {
         this.provideElements(object : ScreenBetterSlotListWidget.ListProvider {
             override fun get(): List<ScreenBetterSlotListEntry> {
-                return VersionList.getProtocols().map { p -> ScreenBetterSlotListEntryProtocol(p) }
+                return VersionList.getProtocols().map { p -> ScreenBetterSlotListEntryProtocol(p, this@ScreenBetterSlotListProtocolHack) }
             }
         })
+
+        specialNames[ProtocolVersion.v1_9_3] = "1.9.3-1.9.4"
+        specialNames[ProtocolVersion.v1_11_1] = "1.11.1-1.11.2"
+        specialNames[ProtocolVersion.v1_16_4] = "1.16.4-1.16.5"
+        specialNames[ProtocolVersion.v1_18] = "1.18-1.18.1"
+        specialNames[ProtocolVersion.v1_19_1] = "1.19.1-1.19.2"
+        specialNames[ProtocolVersion.v1_19_3] = "22w44a"
     }
 
     override fun init() {
@@ -43,10 +52,14 @@ class ScreenBetterSlotListProtocolHack : ScreenBetterSlotList(46, 12) {
         this.renderTitle(matrices, "Protocol Hack")
     }
 
-    class ScreenBetterSlotListEntryProtocol(val protocol: ProtocolVersion) : ScreenBetterSlotListEntry() {
+    class ScreenBetterSlotListEntryProtocol(val protocol: ProtocolVersion, private val parent: ScreenBetterSlotListProtocolHack) : ScreenBetterSlotListEntry() {
 
         override fun renderEntry(matrices: MatrixStack, index: Int, entryWidth: Int, entryHeight: Int, mouseX: Int, mouseY: Int, hovered: Boolean) {
-            RenderUtil.font().textShadow(matrices, VersionList.formatProtocolName(this.protocol), entryWidth / 2F, 0F, if (this.isSelected()) Color.green.rgb else Color.red.rgb, centered = true)
+            var name = protocol.name
+            if (parent.specialNames.containsKey(protocol)) {
+                name = parent.specialNames[protocol]
+            }
+            RenderUtil.font().textShadow(matrices, name, entryWidth / 2F, 0F, if (this.isSelected()) Color.green.rgb else Color.red.rgb, centered = true)
         }
 
         override fun onSingleClickEntry(mouseX: Double, mouseY: Double, mouseButton: Int) {
