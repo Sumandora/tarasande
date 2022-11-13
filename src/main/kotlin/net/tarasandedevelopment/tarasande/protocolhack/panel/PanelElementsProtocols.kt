4c7a0a5e9569565baed7777e14ccb1bb0609b049
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-13 21:45:20 +0100

    Base: fixed owner of protocol hack values

--- src/main/kotlin/net/tarasandedevelopment/tarasande/protocolhack/panel/PanelElementsProtocols.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/protocolhack/panel/PanelElementsProtocols.kt
@@ -2,6 +2,7 @@ package net.tarasandedevelopment.tarasande.protocolhack.panel
 
 import de.florianmichael.viaprotocolhack.util.VersionList
 import net.tarasandedevelopment.tarasande.TarasandeMain
+import net.tarasandedevelopment.tarasande.protocolhack.platform.ProtocolHackValues
 import net.tarasandedevelopment.tarasande.systems.base.valuesystem.impl.meta.ValueSpacer
 import net.tarasandedevelopment.tarasande.systems.base.valuesystem.valuecomponent.ElementValueComponent
 import net.tarasandedevelopment.tarasande.systems.screen.panelsystem.api.PanelElements
@@ -10,8 +11,12 @@ import java.awt.Color
 class PanelElementsProtocols : PanelElements<ElementValueComponent>("Protocol Hack", 100.0, 0.0) {
 
     init {
+        TarasandeMain.managerValue().getValues(ProtocolHackValues).forEach {
+            it.owner = this
+        }
+
         for (protocol in VersionList.getProtocols()) {
-            this.elementList.add(object : ValueSpacer(this, protocol.name, 1.0F) {
+            this.elementList.add(object : ValueSpacer(protocol, protocol.name, 1.0F) {
                 override fun onChange() {
                     TarasandeMain.protocolHack().version.value = protocol.version.toDouble()
                 }
