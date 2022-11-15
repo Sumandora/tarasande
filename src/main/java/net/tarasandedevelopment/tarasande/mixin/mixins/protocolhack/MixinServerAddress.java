e01fb8503cb13f924e30a1eb803ca4f7d60c1139
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-16 00:12:22 +0100

    Protocol hack: fixed Hostname pinging

---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/MixinServerAddress.java
@@ -0,0 +1,25 @@
+package net.tarasandedevelopment.tarasande.mixin.mixins.protocolhack;
+
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import de.florianmichael.viaprotocolhack.util.VersionList;
+import net.minecraft.client.network.AllowedAddressResolver;
+import net.minecraft.client.network.ServerAddress;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerAddress.class)
+public class MixinServerAddress {
+
+    @Shadow @Final private static ServerAddress INVALID;
+
+    @Inject(method = "parse", at = @At("RETURN"), cancellable = true)
+    private static void fixAddress(String address, CallbackInfoReturnable<ServerAddress> cir) {
+        if (!cir.getReturnValue().equals(INVALID) && VersionList.isOlderOrEqualTo(ProtocolVersion.v1_16_4)) {
+            cir.setReturnValue(AllowedAddressResolver.DEFAULT.redirectResolver.lookupRedirect(cir.getReturnValue()).orElse(cir.getReturnValue()));
+        }
+    }
+}
