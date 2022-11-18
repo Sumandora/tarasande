9bce1c6a86bda75de3bd4ab066188239fecab68d
Sumandora <52950755+Sumandora@users.noreply.github.com>
2022-11-18 19:39:28 +0100

    Added FreeCam Camera Position Information

--- src/main/kotlin/net/tarasandedevelopment/tarasande/systems/screen/informationsystem/impl/InformationPlayer.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/systems/screen/informationsystem/impl/InformationPlayer.kt
@@ -17,11 +17,11 @@ class InformationXYZ : Information("Player", "XYZ") {
     private val decimalPlacesZ = ValueNumber(this, "Decimal places: z", 0.0, 1.0, 5.0, 1.0)
 
     override fun getMessage(): String? {
-        if (MinecraftClient.getInstance().player == null) return null
+        val player = MinecraftClient.getInstance().player ?: return null
 
-        return (StringUtil.round(MinecraftClient.getInstance().player?.x!!, this.decimalPlacesX.value.toInt())) + " " +
-                (StringUtil.round(MinecraftClient.getInstance().player?.y!!, this.decimalPlacesY.value.toInt()) + " " +
-                        StringUtil.round(MinecraftClient.getInstance().player?.z!!, this.decimalPlacesZ.value.toInt()))
+        return StringUtil.round(player.x, this.decimalPlacesX.value.toInt()) + " " +
+                StringUtil.round(player.y, this.decimalPlacesY.value.toInt()) + " " +
+                StringUtil.round(player.z, this.decimalPlacesZ.value.toInt())
     }
 }
 
@@ -31,11 +31,11 @@ class InformationNetherXYZ : Information("Player", "Nether XYZ") {
     private val decimalPlacesZ = ValueNumber(this, "Decimal places: z", 0.0, 1.0, 5.0, 1.0)
 
     override fun getMessage(): String? {
-        if (MinecraftClient.getInstance().player == null) return null
+        val player = MinecraftClient.getInstance().player ?: return null
 
-        return (StringUtil.round(MinecraftClient.getInstance().player?.x!! / 8.0, this.decimalPlacesX.value.toInt())) + " " +
-                (StringUtil.round(MinecraftClient.getInstance().player?.y!! / 8.0, this.decimalPlacesY.value.toInt()) + " " +
-                        StringUtil.round(MinecraftClient.getInstance().player?.z!! / 8.0, this.decimalPlacesZ.value.toInt()))
+        return StringUtil.round(player.x / 8.0, this.decimalPlacesX.value.toInt()) + " " +
+                StringUtil.round(player.y / 8.0, this.decimalPlacesY.value.toInt()) + " " +
+                StringUtil.round(player.z / 8.0, this.decimalPlacesZ.value.toInt())
     }
 }
 
@@ -45,9 +45,9 @@ class InformationVelocity : Information("Player", "Velocity") {
     private val decimalPlacesZ = ValueNumber(this, "Decimal places: z", 0.0, 1.0, 5.0, 1.0)
 
     override fun getMessage(): String? {
-        if (MinecraftClient.getInstance().player == null) return null
+        val player = MinecraftClient.getInstance().player ?: return null
 
-        return MinecraftClient.getInstance().player?.velocity?.let {
+        return player.velocity.let {
             StringUtil.round(it.x, decimalPlacesX.value.toInt()) + " " +
                     StringUtil.round(it.y, decimalPlacesY.value.toInt()) + " " +
                     StringUtil.round(it.z, decimalPlacesZ.value.toInt())
@@ -60,9 +60,9 @@ class InformationRotation : Information("Player", "Rotation") {
     private val decimalPlacesPitch = ValueNumber(this, "Decimal places: pitch", 0.0, 1.0, 5.0, 1.0)
 
     override fun getMessage(): String? {
-        if (MinecraftClient.getInstance().player == null) return null
+        val player = MinecraftClient.getInstance().player ?: return null
 
-        return StringUtil.round(MinecraftClient.getInstance().player?.yaw!!.toDouble(), this.decimalPlacesYaw.value.toInt()) + " " + StringUtil.round(MinecraftClient.getInstance().player?.pitch!!.toDouble(), this.decimalPlacesPitch.value.toInt())
+        return StringUtil.round(player.yaw.toDouble(), this.decimalPlacesYaw.value.toInt()) + " " + StringUtil.round(player.pitch.toDouble(), this.decimalPlacesPitch.value.toInt())
     }
 }
 
@@ -71,7 +71,6 @@ class InformationFakeRotation : Information("Player", "Fake Rotation") {
     private val decimalPlacesPitch = ValueNumber(this, "Decimal places: pitch", 0.0, 1.0, 5.0, 1.0)
 
     override fun getMessage(): String? {
-        if (RotationUtil.fakeRotation == null) return null
-        return StringUtil.round(RotationUtil.fakeRotation?.yaw!!.toDouble(), this.decimalPlacesYaw.value.toInt()) + " " + StringUtil.round(RotationUtil.fakeRotation?.pitch!!.toDouble(), this.decimalPlacesPitch.value.toInt())
+        return RotationUtil.fakeRotation?.let { StringUtil.round(it.yaw.toDouble(), this.decimalPlacesYaw.value.toInt()) + " " + StringUtil.round(it.pitch.toDouble(), this.decimalPlacesPitch.value.toInt()) }
     }
 }
