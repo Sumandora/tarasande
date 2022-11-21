9cb2647a322b3dbe75416c58f7538f612830e0b7
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-21 14:14:47 +0100

    Protocol hack: fixed CommandBlocks in 1.9 Protocol 1.8

---
+++ src/main/java/net/tarasandedevelopment/tarasande/mixin/mixins/protocolhack/viaversion/MixinCommandBlockProvider.java
@@ -0,0 +1,15 @@
+package net.tarasandedevelopment.tarasande.mixin.mixins.protocolhack.viaversion;
+
+import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.CommandBlockProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(CommandBlockProvider.class)
+public class MixinCommandBlockProvider {
+
+    @ModifyConstant(method = "sendPermission", constant = @Constant(intValue = 26))
+    public int modifyPermissionLevel(int constant) {
+        return 28;
+    }
+}
