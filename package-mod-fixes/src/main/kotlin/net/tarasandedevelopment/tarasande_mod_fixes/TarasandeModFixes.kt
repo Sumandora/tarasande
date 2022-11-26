47e4f9cedebf3655f53da885015faa35a4f6731b
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-26 17:59:20 +0100

    Package: mod-fixes

---
+++ package-mod-fixes/src/main/kotlin/net/tarasandedevelopment/tarasande_mod_fixes/TarasandeModFixes.kt
@@ -0,0 +1,9 @@
+package net.tarasandedevelopment.tarasande_mod_fixes
+
+import net.fabricmc.api.ClientModInitializer
+
+class TarasandeModFixes : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
