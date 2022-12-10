7d968271bd4803cf906631bfaa04ab32d13c8ab8
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-12-10 21:38:12 +0100

    Protocol hack: fixed DownloadingTerrain 1.19.3 -> 1.19.2

--- package-protocol-hack/src/main/java/de/florianmichael/clampclient/injection/mixin/protocolhack/screen/MixinDownloadingTerrainScreen.java
+++ package-protocol-hack/src/main/java/de/florianmichael/clampclient/injection/mixin/protocolhack/screen/MixinDownloadingTerrainScreen.java
@@ -41,7 +41,10 @@ import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.network.packet.c2s.play.KeepAliveC2SPacket;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -50,6 +53,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(DownloadingTerrainScreen.class)
 public class MixinDownloadingTerrainScreen extends Screen {
 
+    @Shadow @Final private long loadStartTime;
+    @Shadow private boolean closeOnNextTick;
+    @Shadow private boolean ready;
     @Unique
     private int protocolhack_tickCounter;
 
@@ -57,7 +63,7 @@ public class MixinDownloadingTerrainScreen extends Screen {
         super(title);
     }
 
-    @Inject(method = "tick", at = @At("HEAD"))
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void injectTick(CallbackInfo ci) {
         if (VersionList.isOlderOrEqualTo(ProtocolVersion.v1_12_1)) {
             protocolhack_tickCounter++;
@@ -65,6 +71,24 @@ public class MixinDownloadingTerrainScreen extends Screen {
             if (protocolhack_tickCounter % 20 == 0) {
                 MinecraftClient.getInstance().getNetworkHandler().sendPacket(new KeepAliveC2SPacket(0));
             }
+        }
+        if (VersionList.isOlderOrEqualTo(ProtocolVersion.v1_19_1)) {
+            final boolean isTimeOver = this.closeOnNextTick || System.currentTimeMillis() > this.loadStartTime + 2000L;
+
+            if (isTimeOver && this.client != null && this.client.player != null) {
+                final BlockPos blockPos = this.client.player.getBlockPos();
+                final boolean isWorldLoaded = this.client.world != null && this.client.world.isOutOfHeightLimit(blockPos.getY());
+
+                if (isWorldLoaded || this.client.worldRenderer.isRenderingReady(blockPos)) {
+                    this.close();
+                }
+
+                if (this.ready) {
+                    this.closeOnNextTick = true;
+                }
+
+            }
+            ci.cancel();
         }
     }
 }
