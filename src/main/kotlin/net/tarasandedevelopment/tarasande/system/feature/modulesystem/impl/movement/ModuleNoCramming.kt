15fb548c1f9b28425c7ce97007693a73518cde51
Johannes <52950755+Sumandora@users.noreply.github.com>
2022-12-19 21:49:15 +0100

    ModuleNoCramming: Description and default value

--- src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleNoCramming.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleNoCramming.kt
@@ -4,8 +4,12 @@ import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueMode
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.Module
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.ModuleCategory
 
-class ModuleNoCramming : Module("No cramming", "Prevents entity cramming", ModuleCategory.MOVEMENT) {
+class ModuleNoCramming : Module("No cramming", "Force enables/disables entity cramming", ModuleCategory.MOVEMENT) {
 
     val mode = ValueMode(this, "Mode", false, "Force enable", "Force disable")
+
+    init {
+        mode.select(1) // "No cramming" sounds like it would disable it by default...
+    }
 
 }
