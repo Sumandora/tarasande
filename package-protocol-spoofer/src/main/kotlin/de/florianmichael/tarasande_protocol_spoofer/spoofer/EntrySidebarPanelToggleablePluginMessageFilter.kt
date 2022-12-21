b13c6498b05874de6ff7d8ed554576cccda87b7c
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-12-21 15:35:18 +0100

    ProtocolSpoofer: implemented PluginMessageFilter

--- package-protocol-spoofer/src/main/kotlin/de/florianmichael/tarasande_protocol_spoofer/spoofer/EntrySidebarPanelToggleablePluginMessageFilter.kt
+++ package-protocol-spoofer/src/main/kotlin/de/florianmichael/tarasande_protocol_spoofer/spoofer/EntrySidebarPanelToggleablePluginMessageFilter.kt
@@ -1,10 +1,30 @@
 package de.florianmichael.tarasande_protocol_spoofer.spoofer
 
+import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket
+import net.tarasandedevelopment.tarasande.event.EventPacket
+import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueBoolean
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueTextList
 import net.tarasandedevelopment.tarasande.system.screen.screenextensionsystem.sidebar.EntrySidebarPanelToggleable
 import net.tarasandedevelopment.tarasande.system.screen.screenextensionsystem.sidebar.ManagerEntrySidebarPanel
+import su.mandora.event.EventDispatcher
 
 class EntrySidebarPanelToggleablePluginMessageFilter(sidebar: ManagerEntrySidebarPanel) : EntrySidebarPanelToggleable(sidebar, "Plugin message filter", "Spoofer") {
 
-    val channels = ValueTextList(this, "Channels", mutableListOf("fabric"))
+    private val channels = ValueTextList(this, "Channels", mutableListOf("fabric"))
+    private val ignoreCase = ValueBoolean(this, "Ignore case", true)
+
+    init {
+        EventDispatcher.add(EventPacket::class.java) {
+            if (!state.value) return@add
+
+            if (it.type == EventPacket.Type.SEND && it.packet is CustomPayloadC2SPacket) {
+                for (channel in channels.value) {
+                    if ((it.packet as CustomPayloadC2SPacket).channel.toString().contains(channel, ignoreCase.value)) {
+                        it.cancelled = true
+                        break
+                    }
+                }
+            }
+        }
+    }
 }
