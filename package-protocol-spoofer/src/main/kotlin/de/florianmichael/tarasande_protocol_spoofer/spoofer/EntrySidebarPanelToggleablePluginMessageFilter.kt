0099ef8031c8ddad5c7c51423439a3e398ea1937
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-12-21 15:41:34 +0100

    PluginMessageFilter: implemented FilterType

--- package-protocol-spoofer/src/main/kotlin/de/florianmichael/tarasande_protocol_spoofer/spoofer/EntrySidebarPanelToggleablePluginMessageFilter.kt
+++ package-protocol-spoofer/src/main/kotlin/de/florianmichael/tarasande_protocol_spoofer/spoofer/EntrySidebarPanelToggleablePluginMessageFilter.kt
@@ -3,6 +3,7 @@ package de.florianmichael.tarasande_protocol_spoofer.spoofer
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket
 import net.tarasandedevelopment.tarasande.event.EventPacket
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueBoolean
+import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueMode
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueTextList
 import net.tarasandedevelopment.tarasande.system.screen.screenextensionsystem.sidebar.EntrySidebarPanelToggleable
 import net.tarasandedevelopment.tarasande.system.screen.screenextensionsystem.sidebar.ManagerEntrySidebarPanel
@@ -10,6 +11,7 @@ import su.mandora.event.EventDispatcher
 
 class EntrySidebarPanelToggleablePluginMessageFilter(sidebar: ManagerEntrySidebarPanel) : EntrySidebarPanelToggleable(sidebar, "Plugin message filter", "Spoofer") {
 
+    private val filterType = ValueMode(this, "Filter type", false, "Contains", "Equals")
     private val channels = ValueTextList(this, "Channels", mutableListOf("fabric"))
     private val ignoreCase = ValueBoolean(this, "Ignore case", true)
 
@@ -18,8 +20,10 @@ class EntrySidebarPanelToggleablePluginMessageFilter(sidebar: ManagerEntrySideba
             if (!state.value) return@add
 
             if (it.type == EventPacket.Type.SEND && it.packet is CustomPayloadC2SPacket) {
+                val packetChannel = (it.packet as CustomPayloadC2SPacket).channel.toString()
+
                 for (channel in channels.value) {
-                    if ((it.packet as CustomPayloadC2SPacket).channel.toString().contains(channel, ignoreCase.value)) {
+                    if (if (filterType.isSelected(0)) packetChannel.contains(channel, ignoreCase.value) else packetChannel.equals(channel, ignoreCase.value)) {
                         it.cancelled = true
                         break
                     }
