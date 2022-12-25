a851da5591f1a4059258a9bf41dbe8a1fed315e1
Johannes <52950755+Sumandora@users.noreply.github.com>
2022-12-25 23:35:38 +0100

    Disable sneaking fix in 1.14.4 when ModuleSneak#dontSlowdown is enabled

--- package-protocol-hack/src/main/java/de/florianmichael/clampclient/injection/mixin/protocolhack/MixinKeyboardInput.java
+++ package-protocol-hack/src/main/java/de/florianmichael/clampclient/injection/mixin/protocolhack/MixinKeyboardInput.java
@@ -27,6 +27,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.input.Input;
 import net.minecraft.client.input.KeyboardInput;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.tarasandedevelopment.tarasande.TarasandeMain;
+import net.tarasandedevelopment.tarasande.system.feature.modulesystem.impl.movement.ModuleSneak;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -36,6 +38,11 @@ public class MixinKeyboardInput extends Input {
 
     @ModifyVariable(method = "tick", at = @At(value = "LOAD", ordinal = 0), argsOnly = true)
     private boolean injectTick(boolean slowDown) {
+        ModuleSneak moduleSneak = TarasandeMain.Companion.managerModule().get(ModuleSneak.class);
+        if (moduleSneak.getEnabled() && moduleSneak.getDontSlowdown().getValue()) {
+            return slowDown;
+        }
+
         if (VersionList.isOlderOrEqualTo(ProtocolVersion.v1_13_2)) {
             return this.sneaking;
         } else if (VersionList.isOlderOrEqualTo(ProtocolVersion.v1_14_4)) {
--- src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleSneak.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleSneak.kt
@@ -3,19 +3,25 @@ package net.tarasandedevelopment.tarasande.system.feature.modulesystem.impl.move
 import net.tarasandedevelopment.tarasande.event.EventInput
 import net.tarasandedevelopment.tarasande.event.EventKeyBindingIsPressed
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueBoolean
+import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueMode
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.Module
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.ModuleCategory
 import net.tarasandedevelopment.tarasande.util.player.PlayerUtil
 
 class ModuleSneak : Module("Sneak", "Automatically sneaks", ModuleCategory.MOVEMENT) {
 
-    private val standStill = ValueBoolean(this, "Stand still", false)
-    private val dontSlowdown = ValueBoolean(this, "Don't slowdown", false)
+    private val activation = ValueMode(this, "Activation", false, "Manual", "When standing still", "Permanent")
+    @Suppress("MemberVisibilityCanBePrivate") // protocol hack package
+    val dontSlowdown = ValueBoolean(this, "Don't slowdown", false)
 
     init {
         registerEvent(EventKeyBindingIsPressed::class.java) { event ->
             if (event.keyBinding == mc.options.sneakKey)
-                event.pressed = event.pressed || !standStill.value || !PlayerUtil.isPlayerMoving()
+                event.pressed = event.pressed || when {
+                    activation.isSelected(0) -> false
+                    activation.isSelected(1) -> !PlayerUtil.isPlayerMoving()
+                    /*activation.isSelected(2)*/else -> true
+                }
         }
 
         registerEvent(EventInput::class.java) { event ->
