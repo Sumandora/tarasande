4cb66eefe5dce865be0fdf3c7eb67eab0c3f6601
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-12-28 16:52:44 +0100

    NoChatContext: 1.19.3 update

--- src/main/java/net/tarasandedevelopment/tarasande/injection/mixin/feature/module/connection/MixinClientPlayNetworkHandler.java
+++ src/main/java/net/tarasandedevelopment/tarasande/injection/mixin/feature/module/connection/MixinClientPlayNetworkHandler.java
@@ -1,6 +1,8 @@
 package net.tarasandedevelopment.tarasande.injection.mixin.feature.module.connection;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.message.SignedMessage;
 import net.tarasandedevelopment.tarasande.TarasandeMain;
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.impl.exploit.ModuleNoChatContext;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,9 +13,12 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public class MixinClientPlayNetworkHandler {
 
     @ModifyVariable(method = "acknowledge", at = @At("HEAD"), argsOnly = true, index = 2)
-    public boolean hookNoChatContext(boolean value) {
-        if (TarasandeMain.Companion.managerModule().get(ModuleNoChatContext.class).getEnabled())
-            return false;
+    public boolean hookNoChatContext(boolean value, SignedMessage message) {
+        if (TarasandeMain.Companion.managerModule().get(ModuleNoChatContext.class).getEnabled()) {
+            //noinspection DataFlowIssue
+            return message.getSender().equals(MinecraftClient.getInstance().player.getUuid());
+        }
+
         return value;
     }
 }
