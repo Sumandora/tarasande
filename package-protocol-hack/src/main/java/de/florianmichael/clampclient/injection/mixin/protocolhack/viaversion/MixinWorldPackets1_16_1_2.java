100712b62c597f358a78ebf7aa3d50a6951ace6b
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-04 23:33:33 +0100

    ProtocolHack: implemented ViaProxy fixes

---
+++ package-protocol-hack/src/main/java/de/florianmichael/clampclient/injection/mixin/protocolhack/viaversion/MixinWorldPackets1_16_1_2.java
@@ -0,0 +1,15 @@
+package de.florianmichael.clampclient.injection.mixin.protocolhack.viaversion;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(targets = "com.viaversion.viaversion.protocols.protocol1_16_2to1_16_1.packets.WorldPackets$2", remap = false)
+public abstract class MixinWorldPackets1_16_1_2 {
+
+    @ModifyConstant(method = "lambda$registerMap$0", constant = @Constant(intValue = 16))
+    private static int modifySectionCountToSupportClassicWorldHeight(int constant) {
+        return 64;
+    }
+
+}
