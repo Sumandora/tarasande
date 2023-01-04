d258b06073dfa57169cfbe8dc8d35f938686b551
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-04 17:00:18 +0100

    added new HUD Variables updated VLB

--- package-protocol-hack/src/main/kotlin/net/tarasandedevelopment/tarasande_protocol_hack/TarasandeProtocolHack.kt
+++ package-protocol-hack/src/main/kotlin/net/tarasandedevelopment/tarasande_protocol_hack/TarasandeProtocolHack.kt
@@ -21,9 +21,12 @@ import net.minecraft.SharedConstants
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.GameMenuScreen
 import net.raphimc.vialegacy.protocols.classic.protocola1_0_15toc0_28_30.providers.ClassicWorldHeightProvider
+import net.raphimc.vialegacy.protocols.release.protocol1_2_1_3to1_1.storage.SeedStorage
 import net.raphimc.vialegacy.protocols.release.protocol1_3_1_2to1_2_4_5.providers.OldAuthProvider
 import net.raphimc.vialegacy.protocols.release.protocol1_7_2_5to1_6_4.providers.EncryptionProvider
 import net.raphimc.vialegacy.protocols.release.protocol1_8to1_7_6_10.providers.GameProfileFetcher
+import net.raphimc.vialegacy.protocols.release.protocol1_8to1_7_6_10.storage.ChunkTracker
+import net.raphimc.vialegacy.protocols.release.protocol1_8to1_7_6_10.storage.EntityTracker
 import net.tarasandedevelopment.tarasande.TarasandeMain
 import net.tarasandedevelopment.tarasande.event.EventConnectServer
 import net.tarasandedevelopment.tarasande.event.EventDisconnect
@@ -108,6 +111,51 @@ class TarasandeProtocolHack : NativeProvider {
                             val names = (MinecraftClient.getInstance().networkHandler?.connection as? IClientConnection_Protocol)?.protocolhack_getViaConnection()?.protocolInfo?.pipeline?.pipes()?.map { p -> p.javaClass.simpleName } ?: return null
                             if (names.isEmpty()) return null
                             return "\n" + names.subList(0, names.size - 1).joinToString("\n")
+                        }
+                    })
+
+                    add(object : Information("Via Version", VersionListEnum.r1_7_6tor1_7_10.name + " Entity Tracker") {
+                        override fun getMessage(): String? {
+                            if (viaConnection!!.has(EntityTracker::class.java)) {
+                                return viaConnection!!.get(EntityTracker::class.java)?.trackedEntities?.size.toString()
+                            }
+                            return null
+                        }
+                    })
+
+                    add(object : Information("Via Version", VersionListEnum.r1_7_6tor1_7_10.name + " Virtual Holograms") {
+                        override fun getMessage(): String? {
+                            if (viaConnection!!.has(EntityTracker::class.java)) {
+                                return viaConnection!!.get(EntityTracker::class.java)?.virtualHolograms?.size.toString()
+                            }
+                            return null
+                        }
+                    })
+
+                    add(object : Information("Via Version", VersionListEnum.r1_5_2.name + " Entity Tracker") {
+                        override fun getMessage(): String? {
+                            if (viaConnection!!.has(net.raphimc.vialegacy.protocols.release.protocol1_6_1to1_5_2.storage.EntityTracker::class.java)) {
+                                return viaConnection!!.get(net.raphimc.vialegacy.protocols.release.protocol1_6_1to1_5_2.storage.EntityTracker::class.java)?.trackedEntities?.size.toString()
+                            }
+                            return null
+                        }
+                    })
+
+                    add(object : Information("Via Version", VersionListEnum.r1_2_4tor1_2_5.name + " Entity Tracker") {
+                        override fun getMessage(): String? {
+                            if (viaConnection!!.has(net.raphimc.vialegacy.protocols.release.protocol1_3_1_2to1_2_4_5.storage.EntityTracker::class.java)) {
+                                return viaConnection!!.get(net.raphimc.vialegacy.protocols.release.protocol1_3_1_2to1_2_4_5.storage.EntityTracker::class.java)?.trackedEntities?.size.toString()
+                            }
+                            return null
+                        }
+                    })
+
+                    add(object : Information("Via Version", VersionListEnum.r1_1.name + " World Seed") {
+                        override fun getMessage(): String? {
+                            if (viaConnection!!.has(SeedStorage::class.java)) {
+                                return viaConnection!!.get(SeedStorage::class.java)?.seed.toString()
+                            }
+                            return null
                         }
                     })
                 }
