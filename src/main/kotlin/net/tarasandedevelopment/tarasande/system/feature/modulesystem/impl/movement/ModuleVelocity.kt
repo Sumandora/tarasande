4bfa5d3efb26e2af47160e6bd0c3b58624042637
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-01-10 20:01:11 +0100

    I might have mild mental retardation

--- src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleVelocity.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/system/feature/modulesystem/impl/movement/ModuleVelocity.kt
@@ -48,7 +48,7 @@ class ModuleVelocity : Module("Velocity", "Reduces knockback", ModuleCategory.MO
 
     init {
         registerEvent(EventVelocity::class.java) { event ->
-            if (ThreadLocalRandom.current().nextInt(100) <= chance.value) return@registerEvent
+            if (ThreadLocalRandom.current().nextInt(100) > chance.value) return@registerEvent
             if (!packets.isSelected(event.packet.ordinal)) return@registerEvent
             if (event.velocityX == 0.0 && event.velocityY == 0.0 && event.velocityZ == 0.0)
                 return@registerEvent // wtf?
