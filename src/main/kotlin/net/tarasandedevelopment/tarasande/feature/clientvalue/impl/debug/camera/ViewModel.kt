fefd8affea2ece120bb13afd0846e8f27b9d3fd9
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-18 15:47:19 +0100

    some additions to the ViewModel debugger

--- src/main/kotlin/net/tarasandedevelopment/tarasande/feature/clientvalue/impl/debug/camera/ViewModel.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/feature/clientvalue/impl/debug/camera/ViewModel.kt
@@ -2,7 +2,6 @@ package net.tarasandedevelopment.tarasande.feature.clientvalue.impl.debug.camera
 
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Arm
-import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueBoolean
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueMode
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueNumber
 import net.tarasandedevelopment.tarasande.util.math.MathUtil.roundAwayFromZero
@@ -23,20 +22,20 @@ class ViewModel {
     private val scaleY = ValueNumber(this, "Scale Y", 0.0, 1.0, 2.0, 0.1)
     private val scaleZ = ValueNumber(this, "Scale Z", 0.0, 1.0, 2.0, 0.1)
 
-    private val arms = ValueMode(this, "Arms", true, *Arm.values().map { StringUtil.formatEnumTypes(it.name) }.toTypedArray())
-    val applyToHand = ValueBoolean(this, "Apply to hand", true)
+    private val hands = ValueMode(this, "Hands", true, *Arm.values().map { StringUtil.formatEnumTypes(it.name) }.toTypedArray())
+    private val emptyHands = ValueMode(this, "Empty hands", true, *Arm.values().map { StringUtil.formatEnumTypes(it.name) }.toTypedArray())
 
-    fun applyTransform(matrices: MatrixStack, arm: Arm) {
-        if (!arms.isSelected(arm.ordinal)) return
-        val handOffset = -roundAwayFromZero(arm.opposite /*Let's make the user input the main hand*/.ordinal - 0.5) /* rofl */
-        matrices.translate(x.value * handOffset, y.value, z.value)
-        matrices.multiply(
-            Quaternionf()
-                .rotateX(Math.toRadians(rotateX.value).toFloat())
-                .rotateY(Math.toRadians(rotateY.value * handOffset).toFloat())
-                .rotateZ(Math.toRadians(rotateZ.value).toFloat())
-        )
-        matrices.scale(scaleX.value.toFloat(), scaleY.value.toFloat(), scaleZ.value.toFloat())
+    fun applyTransform(matrices: MatrixStack, emptyHand: Boolean, arm: Arm) {
+        if ((!emptyHand && hands.isSelected(arm.ordinal)) || (emptyHand && emptyHands.isSelected(arm.ordinal))) {
+            val handOffset = -roundAwayFromZero(arm.opposite /*Let's make the user input the main hand*/.ordinal - 0.5) /* rofl */
+            matrices.translate(x.value * handOffset, y.value, z.value)
+            matrices.multiply(
+                Quaternionf()
+                    .rotateX(Math.toRadians(rotateX.value).toFloat())
+                    .rotateY(Math.toRadians(rotateY.value * handOffset).toFloat())
+                    .rotateZ(Math.toRadians(rotateZ.value).toFloat())
+            )
+            matrices.scale(scaleX.value.toFloat(), scaleY.value.toFloat(), scaleZ.value.toFloat())
+        }
     }
-
 }
