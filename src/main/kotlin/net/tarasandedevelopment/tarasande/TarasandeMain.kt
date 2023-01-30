ca4820734f39f606f97399ebf74bf5913eddad30
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-30 12:45:29 +0100

    updated TODO

--- src/main/kotlin/net/tarasandedevelopment/tarasande/TarasandeMain.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/TarasandeMain.kt
@@ -21,7 +21,7 @@ import java.util.logging.Logger
 /**
  * TODO AND BUG TRACKER FOR TARASANDE - (c) Johannes and FlorianMichael
  *
- * TODO | General:
+ * TODO | Base:
  *  - User configs
  *  - Crystal Aura
  *  - Change Name/Skin/Cape in Account Manager
@@ -31,13 +31,20 @@ import java.util.logging.Logger
  *  - AutoLog
  *
  * TODO | Protocol Hack:
- *  - Entity Dimensions in <= 1.19.2
+ *  - Entity Dimensions are missing
+ *  - Entity offset "fix" is a bit cursed
  *  - WindowClick is not fully remapped in <= 1.16.5
- *  - Fix chest rendering and collisions in <= b1.8
- *  - Reimplement sword actions in <= b1.7.3
- *  - Change Boat and Chest Model in <= 1.8
- *  - Bounding boxes of Fences in <= b1.5
- *  - Blink Position Tracker in 1.8
+ *
+ * TODO | Crasher:
+ *  - Cleanup crasher (Velocity is outdated)
+ *  - Basic crasher module
+ *
+ * TODO | Protocol Spoofer:
+ *  - Add basic exploits from bash for Forge
+ *
+ * TODO | Litematica:
+ *  - Implement PixelArt generator
+ *  - Implement head and skin generator
  */
 
 const val TARASANDE_NAME = "tarasande" // "lowercase gang" ~kennytv
