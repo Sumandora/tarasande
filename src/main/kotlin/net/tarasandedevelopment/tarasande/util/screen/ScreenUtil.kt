ce55490672378008ab0b87bfcde39c4e89775c56
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-02-11 18:42:46 +0100

    Make code checker happy

--- src/main/kotlin/net/tarasandedevelopment/tarasande/util/screen/ScreenUtil.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/util/screen/ScreenUtil.kt
@@ -7,7 +7,7 @@ import net.tarasandedevelopment.tarasande.mc
 object ScreenUtil {
 
     fun playClickSound() {
-        mc.soundManager.play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0f))
+        mc.soundManager.play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0F))
     }
 
 }
