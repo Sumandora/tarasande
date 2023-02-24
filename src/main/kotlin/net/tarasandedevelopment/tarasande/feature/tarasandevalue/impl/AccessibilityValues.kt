6837a6cb8bf329e12491be0d8844620159ce7597
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-02-24 23:51:17 +0100

    removed unused value

--- src/main/kotlin/net/tarasandedevelopment/tarasande/feature/tarasandevalue/impl/AccessibilityValues.kt
+++ src/main/kotlin/net/tarasandedevelopment/tarasande/feature/tarasandevalue/impl/AccessibilityValues.kt
@@ -1,12 +1,10 @@
 package net.tarasandedevelopment.tarasande.feature.tarasandevalue.impl
 
-import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueBoolean
 import net.tarasandedevelopment.tarasande.system.base.valuesystem.impl.ValueNumber
 
 object AccessibilityValues {
 
     val doubleClickDelay = ValueNumber(this, "Double click delay", 0.0, 300.0, 1000.0, 100.0)
-    val playSoundWhenClickingInSidebar = ValueBoolean(this, "Play sound when clicking in sidebar", true)
     val statusRenderTime = ValueNumber(this, "Status render time", 0.0, 2000.0, 10000.0, 500.0)
 
 }
