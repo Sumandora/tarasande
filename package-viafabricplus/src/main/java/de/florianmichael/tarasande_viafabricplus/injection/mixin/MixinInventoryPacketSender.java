f81997f2524ffdc562dd478c84a9b382bdd65d3e
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-02-26 23:00:18 +0100

    updated ViaFabricPlus implementation

--- package-viafabricplus/src/main/java/de/florianmichael/tarasande_viafabricplus/injection/mixin/MixinInventoryPacketSender.java
+++ package-viafabricplus/src/main/java/de/florianmichael/tarasande_viafabricplus/injection/mixin/MixinInventoryPacketSender.java
@@ -2,7 +2,6 @@ package de.florianmichael.tarasande_viafabricplus.injection.mixin;
 
 import de.florianmichael.tarasande_viafabricplus.TarasandeViaFabricPlus;
 import de.florianmichael.viafabricplus.definition.v1_8_x.InventoryPacketSender;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.ManagerModule;
 import net.tarasandedevelopment.tarasande.system.feature.modulesystem.impl.movement.ModuleInventoryMove;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,11 +9,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(InventoryPacketSender.class)
+@Mixin(value = InventoryPacketSender.class, remap = false)
 public class MixinInventoryPacketSender {
 
     @Inject(method = "sendOpenInventoryAchievement", at = @At("HEAD"), cancellable = true)
-    private static void hookInventoryMove(ClientPlayNetworkHandler clientPlayNetworkHandler, CallbackInfo ci) {
+    private static void hookInventoryMove(CallbackInfo ci) {
         if (ManagerModule.INSTANCE.get(ModuleInventoryMove.class).getEnabled().getValue() && TarasandeViaFabricPlus.cancelOpenPacket.getValue()) {
             ci.cancel();
         }
--- package-viafabricplus/src/main/kotlin/de/florianmichael/tarasande_viafabricplus/TarasandeViaFabricPlus.kt
+++ package-viafabricplus/src/main/kotlin/de/florianmichael/tarasande_viafabricplus/TarasandeViaFabricPlus.kt
@@ -3,9 +3,9 @@ package de.florianmichael.tarasande_viafabricplus
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion
 import de.florianmichael.tarasande_viafabricplus.tarasandevalues.EveryItemOnArmor
 import de.florianmichael.viafabricplus.definition.v1_8_x.IdlePacketExecutor
-import de.florianmichael.viafabricplus.platform.ProtocolRange
-import de.florianmichael.viafabricplus.value.ValueHolder
+import de.florianmichael.viafabricplus.setting.groups.DebugSettings
 import de.florianmichael.vialoadingbase.ViaLoadingBase
+import de.florianmichael.vialoadingbase.platform.ProtocolRange
 import net.fabricmc.api.ClientModInitializer
 import net.tarasandedevelopment.tarasande.event.EventDispatcher
 import net.tarasandedevelopment.tarasande.event.impl.EventSuccessfulLoad
@@ -28,7 +28,7 @@ class TarasandeViaFabricPlus : ClientModInitializer {
             cancelOpenPacket = ValueBoolean(ManagerModule.get(ModuleInventoryMove::class.java), "Cancel open packet (" + ProtocolRange.andOlder(ProtocolVersion.v1_11_1) + ")", false, isEnabled = { ViaLoadingBase.getTargetVersion().isOlderThanOrEqualTo(ProtocolVersion.v1_11_1) })
 
             ManagerModule.get(ModuleTickBaseManipulation::class.java).apply {
-                val chargeOnIdlePacketSkip = ValueBoolean(this, "Charge on idle packet skip (" + ValueHolder.sendIdlePacket.protocolRange + ")", false, isEnabled = { !ValueHolder.sendIdlePacket.value })
+                val chargeOnIdlePacketSkip = ValueBoolean(this, "Charge on idle packet skip (" + DebugSettings.getClassWrapper().sendIdlePacket.protocolRange + ")", false, isEnabled = { !DebugSettings.getClassWrapper().sendIdlePacket.value })
 
                 IdlePacketExecutor.registerIdlePacketSkipExecute {
                     if (chargeOnIdlePacketSkip.isEnabled() && chargeOnIdlePacketSkip.value)
