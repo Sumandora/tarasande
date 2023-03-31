96cd1afacea80c9564d448091850da75776d8d1f
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-03-31 19:38:40 +0200

    Reset color after EventRender3D

--- src/main/java/net/tarasandedevelopment/tarasande/injection/mixin/event/MixinWorldRenderer.java
+++ src/main/java/net/tarasandedevelopment/tarasande/injection/mixin/event/MixinWorldRenderer.java
@@ -1,5 +1,6 @@
 package net.tarasandedevelopment.tarasande.injection.mixin.event;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
@@ -19,5 +20,6 @@ public class MixinWorldRenderer {
     @Inject(method = "render", at = @At("TAIL"))
     public void hookEventRender3D(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
         EventDispatcher.INSTANCE.call(new EventRender3D(matrices, positionMatrix));
+        RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
     }
 }
