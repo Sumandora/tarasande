9811aa7742981d9dcf3ab9dd85e33ee62cc29ce4
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-08-26 11:50:25 +0200

    More accurate description for ModuleAntiPowderSnow

--- src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleAntiPowderSnow.kt
+++ src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleAntiPowderSnow.kt
@@ -3,4 +3,4 @@ package su.mandora.tarasande.system.feature.modulesystem.impl.movement
 import su.mandora.tarasande.system.feature.modulesystem.Module
 import su.mandora.tarasande.system.feature.modulesystem.ModuleCategory
 
-class ModuleAntiPowderSnow : Module("Anti powder snow", "Always walk on powder snow", ModuleCategory.MOVEMENT)
+class ModuleAntiPowderSnow : Module("Anti powder snow", "Allows to walk on powder snow without boots", ModuleCategory.MOVEMENT)
