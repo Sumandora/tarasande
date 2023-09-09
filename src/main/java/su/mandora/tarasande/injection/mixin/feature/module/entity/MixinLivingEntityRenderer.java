3a55f9f6c6272699514e5d797c874b349012e211
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-09 19:22:48 +0200

    Simplify MixinLivingEntityRenderer and reduce Redirect to ModifyArg

--- src/main/java/su/mandora/tarasande/injection/mixin/feature/module/entity/MixinLivingEntityRenderer.java
+++ src/main/java/su/mandora/tarasande/injection/mixin/feature/module/entity/MixinLivingEntityRenderer.java
@@ -1,12 +1,8 @@
 package su.mandora.tarasande.injection.mixin.feature.module.entity;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
@@ -14,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import su.mandora.tarasande.injection.accessor.IEntity;
@@ -22,17 +18,13 @@ import su.mandora.tarasande.system.feature.modulesystem.ManagerModule;
 import su.mandora.tarasande.system.feature.modulesystem.impl.render.ModuleTrueSight;
 
 @Mixin(LivingEntityRenderer.class)
-public abstract class MixinLivingEntityRenderer<T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> {
+public abstract class MixinLivingEntityRenderer<T extends LivingEntity> {
 
     @Unique
     private T tarasande_livingEntity;
 
-    protected MixinLivingEntityRenderer(EntityRendererFactory.Context ctx) {
-        super(ctx);
-    }
-
     @Inject(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"))
-    public void modifyYaw(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
+    public void captureEntity(T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         this.tarasande_livingEntity = livingEntity;
     }
 
@@ -42,15 +34,14 @@ public abstract class MixinLivingEntityRenderer<T extends LivingEntity, M extend
             cir.setReturnValue(true);
     }
 
-    @Redirect(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
-    public void hookTrueSight(EntityModel<?> instance, MatrixStack matrixStack, VertexConsumer vertexConsumer, int a, int b, float c, float d, float e, float f) {
+    @ModifyArg(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"), index = 7)
+    public float hookTrueSight(float transparency) {
         ModuleTrueSight moduleTrueSight = ManagerModule.INSTANCE.get(ModuleTrueSight.class);
         if (moduleTrueSight.getEnabled().getValue()) {
-            IEntity accessor = (IEntity) tarasande_livingEntity;
-            if (tarasande_livingEntity.isInvisibleTo(MinecraftClient.getInstance().player) || accessor.tarasande_forceGetFlag(Entity.INVISIBLE_FLAG_INDEX))
-                f = (float) moduleTrueSight.getAlpha().getValue();
+            if (tarasande_livingEntity.isInvisibleTo(MinecraftClient.getInstance().player) || ((IEntity) tarasande_livingEntity).tarasande_forceGetFlag(Entity.INVISIBLE_FLAG_INDEX))
+                transparency = (float) moduleTrueSight.getAlpha().getValue();
         }
 
-        instance.render(matrixStack, vertexConsumer, a, b, c, d, e, f);
+        return transparency;
     }
 }
