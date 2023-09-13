f137bfe492a434238291890af7b97bbede22aa15
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-13 10:14:21 +0200

    Rewrite ModuleSafeWalk to randomize its values and make it use the new input interface

--- src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleSafeWalk.kt
+++ src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleSafeWalk.kt
@@ -1,10 +1,12 @@
 package su.mandora.tarasande.system.feature.modulesystem.impl.movement
 
-import su.mandora.tarasande.event.impl.EventKeyBindingIsPressed
+import net.minecraft.entity.MovementType
+import net.minecraft.entity.player.PlayerEntity
+import su.mandora.tarasande.event.impl.EventInput
 import su.mandora.tarasande.mc
 import su.mandora.tarasande.system.base.valuesystem.impl.ValueBoolean
 import su.mandora.tarasande.system.base.valuesystem.impl.ValueMode
-import su.mandora.tarasande.system.base.valuesystem.impl.ValueNumber
+import su.mandora.tarasande.system.base.valuesystem.impl.ValueNumberRange
 import su.mandora.tarasande.system.feature.modulesystem.Module
 import su.mandora.tarasande.system.feature.modulesystem.ModuleCategory
 import su.mandora.tarasande.util.player.prediction.PredictionEngine
@@ -15,22 +17,27 @@ class ModuleSafeWalk : Module("Safe walk", "Prevents falling off blocks", Module
     val clipInAir = ValueBoolean(this, "Clip in air", false)
     val sneak = ValueBoolean(this, "Sneak", false)
     private val offGround = ValueMode(this, "Off-ground", false, "Force disable", "Force enable", "Ignore", isEnabled = { sneak.value })
-    private val extrapolation = ValueNumber(this, "Extrapolation", 0.0, 1.0, 10.0, 1.0, isEnabled = { sneak.value })
-    private val unsneakDelay = ValueNumber(this, "Unsneak delay", 0.0, 0.0, 10.0, 1.0, isEnabled = { sneak.value })
+    private val extrapolation = ValueNumberRange(this, "Extrapolation", 0.0, 1.0, 3.0, 10.0, 1.0, isEnabled = { sneak.value })
+    private val unsneakDelay = ValueNumberRange(this, "Unsneak delay", 0.0, 1.0, 3.0, 10.0, 1.0, isEnabled = { sneak.value })
 
     private var lastSneak = Int.MIN_VALUE
 
-    init {
-        registerEvent(EventKeyBindingIsPressed::class.java) { event -> // TODO Fix
-            if(event.keyBinding != mc.options.sneakKey)
-                return@registerEvent
+    private var nextUnsneakDelay = 0
+    private var nextExtrapolation = 0
 
-            if(mc.player == null)
+    init {
+        nextExtrapolation = extrapolation.randomNumber().toInt()
+        nextUnsneakDelay = unsneakDelay.randomNumber().toInt()
+    }
+
+    init {
+        registerEvent(EventInput::class.java) { event ->
+            if(event.input != mc.player?.input)
                 return@registerEvent
 
             if (sneak.value && mc.player!!.velocity.horizontalLengthSquared() > 0.0) {
-                val onEdge = PredictionEngine.predictState(extrapolation.value.toInt(), input = mc.player!!.input.with(sneaking = false)).first.let { prediction ->
-                    prediction.y < mc.player!!.y
+                val onEdge = PredictionEngine.predictState(nextExtrapolation, input = mc.player!!.input.with(sneaking = true)).first.let { prediction ->
+                    mc.player!!.velocity.let { (prediction as PlayerEntity).adjustMovementForSneaking(it, MovementType.SELF) != it }
                 }
                 val forceSneak = when {
                     offGround.isSelected(0) -> mc.player!!.isOnGround && onEdge
@@ -39,10 +46,13 @@ class ModuleSafeWalk : Module("Safe walk", "Prevents falling off blocks", Module
                     else -> true
                 }
 
-                if (forceSneak)
+                if (forceSneak) {
                     lastSneak = mc.player!!.age
+                    nextExtrapolation = extrapolation.randomNumber().toInt()
+                    nextUnsneakDelay = unsneakDelay.randomNumber().toInt()
+                }
 
-                event.pressed = event.pressed || forceSneak || (mc.player!!.age - lastSneak < unsneakDelay.value)
+                event.input.sneaking = event.input.sneaking || forceSneak || (mc.player!!.age - lastSneak < nextUnsneakDelay)
             }
         }
     }
