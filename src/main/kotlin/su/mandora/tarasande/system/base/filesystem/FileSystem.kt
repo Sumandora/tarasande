8c2b100c285e651f6d5b7b38ebb9628a6ac4b718
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-25 17:49:01 +0200

    Remove backup files since they are no longer needed

--- src/main/kotlin/su/mandora/tarasande/system/base/filesystem/FileSystem.kt
+++ src/main/kotlin/su/mandora/tarasande/system/base/filesystem/FileSystem.kt
@@ -6,68 +6,62 @@ import su.mandora.tarasande.TARASANDE_NAME
 import su.mandora.tarasande.event.EventDispatcher
 import su.mandora.tarasande.event.impl.EventShutdown
 import su.mandora.tarasande.gson
-import su.mandora.tarasande.logger
-import java.util.logging.Level
+
+// I don't like this name collision, but I can't really come up with better names for this... TODO
+
+typealias JavaFile = java.io.File
 
 object ManagerFile : Manager<File>() {
 
     @Suppress("MemberVisibilityCanBePrivate") // Packages could want that
-    val rootDirectory = java.io.File(System.getProperty("user.home") + java.io.File.separator + TARASANDE_NAME)
+    val rootDirectory = JavaFile(System.getProperty("user.home") + JavaFile.separator + TARASANDE_NAME)
 
     init {
         EventDispatcher.add(EventShutdown::class.java) {
-            save(true)
+            saveAll()
         }
     }
 
     override fun insert(obj: File, index: Int) {
         super.insert(obj, index)
-        load(obj)
-    }
-
-    fun save(backup: Boolean) {
-        for (file in list) {
-            val fileObj = java.io.File(rootDirectory, file.name)
-
-            if (!fileObj.parentFile.exists())
-                fileObj.parentFile.mkdirs()
-
-            if (fileObj.exists() && backup)
-                fileObj.renameTo(java.io.File(fileObj.path + "_backup"))
-
-            fileObj.writeText(gson.toJson(file.save()))
-        }
-    }
-
-    fun load(file: File) {
         try {
-            internalLoad(file, false)
+            obj.loadFile()
         } catch (t: Throwable) {
             t.printStackTrace()
-            try {
-                internalLoad(file, true)
-            } catch (t: Throwable) {
-                t.printStackTrace()
-                logger.log(Level.CONFIG, file.name + " didn't load correctly!")
-            }
         }
     }
 
-    private fun internalLoad(file: File, backup: Boolean) {
-        val fileObj = java.io.File(rootDirectory, file.name + (if (backup) "_backup" else ""))
-        if (fileObj.exists()) {
-            val content = fileObj.readBytes().decodeToString()
-            val jsonElement = gson.fromJson(content, JsonElement::class.java)
-            file.load(jsonElement)
+    fun saveAll() {
+        for (file in list) {
+            file.saveFile()
         }
-        file.loaded = true
     }
 }
 
 abstract class File(val name: String) {
 
-    var loaded = false
-
     abstract fun save(): JsonElement
     abstract fun load(jsonElement: JsonElement)
+
+    fun getPath() = JavaFile(ManagerFile.rootDirectory, name)
+
+    fun saveFile() {
+        val fileObj = getPath()
+
+        if (!fileObj.parentFile.exists())
+            fileObj.parentFile.mkdirs()
+
+        fileObj.writeText(gson.toJson(save()))
+    }
+
+    fun loadFile() {
+        val fileObj = getPath()
+
+        if (!fileObj.exists())
+            return
+
+        val content = fileObj.readBytes().decodeToString()
+        val jsonElement = gson.fromJson(content, JsonElement::class.java)
+        load(jsonElement)
+    }
 }
