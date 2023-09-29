ef37306f26d6a6b38957e9d2900f2629deddbe63
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-29 16:35:42 +0200

    ScreenBetter functionality has been merged into base game, remove that part

--- src/main/kotlin/su/mandora/tarasande/feature/screen/ScreenBetter.kt
+++ src/main/kotlin/su/mandora/tarasande/feature/screen/ScreenBetter.kt
@@ -1,17 +1,10 @@
 package su.mandora.tarasande.feature.screen
 
-import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.text.Text
 
 open class ScreenBetter(title: String, var prevScreen: Screen?) : Screen(Text.of(title)) {
-
     override fun close() {
         client!!.setScreen(prevScreen)
-    }
-
-    override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
-        renderBackground(context)
-        super.render(context, mouseX, mouseY, delta)
     }
 }
