40209cf50706343e4700c2eb1d133487669c2686
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-09-29 16:37:38 +0200

    Update mixins to reflect the new class names

--- src/main/java/su/mandora/tarasande/injection/mixin/core/screen/accountmanager/MixinProfileKeys.java
+++ src/main/java/su/mandora/tarasande/injection/mixin/core/screen/accountmanager/MixinProfileKeys.java
@@ -1,7 +1,7 @@
 package su.mandora.tarasande.injection.mixin.core.screen.accountmanager;
 
-import net.minecraft.client.util.ProfileKeys;
-import net.minecraft.client.util.Session;
+import net.minecraft.client.session.ProfileKeys;
+import net.minecraft.client.session.Session;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ProfileKeys.class)
 public interface MixinProfileKeys {
 
-    @Redirect(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Session;getAccountType()Lnet/minecraft/client/util/Session$AccountType;"))
+    @Redirect(method = "create", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/session/Session;getAccountType()Lnet/minecraft/client/session/Session$AccountType;"))
     private static Session.AccountType forceKeyCreation(Session instance) {
         return Session.AccountType.MSA;
     }
--- src/main/java/su/mandora/tarasande/injection/mixin/core/screen/accountmanager/MixinYggdrasilAuthenticationService.java
+++ src/main/java/su/mandora/tarasande/injection/mixin/core/screen/accountmanager/MixinYggdrasilAuthenticationService.java
@@ -1,8 +1,11 @@
 package su.mandora.tarasande.injection.mixin.core.screen.accountmanager;
 
+import com.mojang.authlib.Environment;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import su.mandora.tarasande.TarasandeMainKt;
@@ -10,9 +13,11 @@ import su.mandora.tarasande.TarasandeMainKt;
 @Mixin(value = YggdrasilAuthenticationService.class, remap = false)
 public class MixinYggdrasilAuthenticationService {
 
-    @Redirect(method = "<init>(Ljava/net/Proxy;Ljava/lang/String;Lcom/mojang/authlib/Environment;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;)V"))
-    private void silentInit(Logger instance, String s) {
-        if(!s.endsWith("name='" + TarasandeMainKt.TARASANDE_NAME + "'"))
+    @Shadow @Final private Environment environment;
+
+    @Redirect(method = "<init>(Ljava/net/Proxy;Lcom/mojang/authlib/Environment;)V", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;info(Ljava/lang/String;Ljava/lang/Object;)V"))
+    private void silentInit(Logger instance, String s, Object o) {
+        if(!environment.name().equals(TarasandeMainKt.TARASANDE_NAME))
             instance.info(s);
     }
 
