b20ea0109ea0def2c07e465c1ec0a74fe37288c7
johannes <johannesmiesenhardt@gmail.com>
2023-12-10 16:33:26 +0100

    Fix ModuleTridentBoost#multiplier

--- src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleTridentBoost.kt
+++ src/main/kotlin/su/mandora/tarasande/system/feature/modulesystem/impl/movement/ModuleTridentBoost.kt
@@ -7,7 +7,7 @@ import su.mandora.tarasande.system.feature.modulesystem.ModuleCategory
 
 class ModuleTridentBoost : Module("Trident boost", "Boosts you when using riptide with a trident", ModuleCategory.MOVEMENT) {
 
-    val multiplier = ValueNumber(this, "Multiplier", 0.1, 2.0, 1.0, 1E-2)
+    val multiplier = ValueNumber(this, "Multiplier", 0.1, 1.0, 2.0, 0.1)
     val allowOutOfWater = ValueBoolean(this, "Allow out of water", true)
 
 }
