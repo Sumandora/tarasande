4dbbef2c7212fe603b2b8665bc36d0b3af2c4765
johannes <johannesmiesenhardt@gmail.com>
2024-03-10 02:32:03 +0100

    Update chat remove maximum history injection

--- src/main/java/su/mandora/tarasande/injection/mixin/feature/tarasandevalue/MixinChatHud.java
+++ src/main/java/su/mandora/tarasande/injection/mixin/feature/tarasandevalue/MixinChatHud.java
@@ -1,18 +1,25 @@
 package su.mandora.tarasande.injection.mixin.feature.tarasandevalue;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.gui.hud.ChatHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import su.mandora.tarasande.feature.tarasandevalue.impl.debug.Chat;
 
-import java.util.List;
-
 @Mixin(ChatHud.class)
 public class MixinChatHud {
 
-    @WrapWithCondition(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At(value = "INVOKE", target = "Ljava/util/List;remove(I)Ljava/lang/Object;"))
-    public boolean removeHistoryMaximum(List<?> instance, int i) {
-        return !Chat.INSTANCE.getRemoveMaximum().isSelected(0);
+    @ModifyExpressionValue(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 2))
+    public int removeHistoryMaximum_visibles(int original) {
+        if(Chat.INSTANCE.getRemoveMaximum().isSelected(0))
+            return 0;
+        return original;
+    }
+
+    @ModifyExpressionValue(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 4))
+    public int removeHistoryMaximum(int original) {
+        if(Chat.INSTANCE.getRemoveMaximum().isSelected(0))
+            return 0;
+        return original;
     }
 }
